507794886de51f2e7aec051f9b786f13aef90f2f
Joshua Terry <jterry@ddi.org>
2016-12-07 14:56:38 -0500

    DC-63 Split Enums in to Different classes in their own happy special directory etc.

---
+++ DDI/DDI.Business/Enums/PaymentMethodEnum.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDI.Business.Enums
+{
+    public class PaymentMethodEnum
+    {
+        public enum PaymentMethod { None = 0, Check = 1, ACH = 2, Wire = 3, SWIFT = 4 }
+    }
+}
