884ed2e83407d76a1dd136a3b1969aff1f1b3e6f
Joshua Terry <jterry@ddi.org>
2016-12-07 14:50:56 -0500

    DC-63 Split Enums into a class

---
+++ DDI/DDI.Business/DataModels/Enums.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DDI.Business.DataModels
+{
+    public class Enums
+    {
+        public enum PaymentMethod { None = 0, Check = 1, ACH = 2, Wire = 3, SWIFT = 4 }
+    }
+}
