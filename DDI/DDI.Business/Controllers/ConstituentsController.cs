c6c6bd400f046e49a6dd4b7685ab0a17de9126c8
Ryan Clough <w.ryan.clough@gmail.com>
2016-12-07 15:52:27 -0500

    DC-37: added Constituent controller and set data return type to JSON, need to add service to do data acess

---
+++ DDI/DDI.Business/Controllers/ConstituentsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace DDI.Business.Controllers
+{
+    [Authorize]
+    public class ConstituentsController : ApiController
+    {
+        // GET api/constituents
+        public IHttpActionResult GetConstituents()
+        {
+            return Ok();
+        }
+
+        // GET api/constituents/5
+        public IHttpActionResult GetConstituent(int id)
+        {
+            return Ok();
+        }
+
+        // POST api/constituents
+        public IHttpActionResult Post([FromBody]string value)
+        {
+            return Ok();
+        }
+
+        // PUT api/constituents/5
+        public IHttpActionResult Put(int id, [FromBody]string value)
+        {
+            return Ok();
+        }
+
+        // DELETE api/constituents/5
+        public IHttpActionResult Delete(int id)
+        {
+            return Ok();
+        }
+    }
+}
