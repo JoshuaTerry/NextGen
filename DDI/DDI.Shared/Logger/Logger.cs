c382f784ee10c791450f9da1556fb20ebe9a4412
DDI\pevans <pevans@ddi.org>
2016-12-09 12:27:46 -0500

    DC-61: log4net implementation.  Created Logger file in shared project to be called from all libraries to perform logging.  Logger uses log4net, but that is now encapsulted so the logging library can be switched out if needed.

---
+++ DDI/DDI.Shared/Logger/Logger.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using log4net;
+
+namespace DDI.Shared.Logger
+{
+	/// <summary>
+	/// Manages log message creation.
+	/// </summary>
+	/// <remarks>Acts as a wrapper to encapsulate the actual logging library being used (log4net).</remarks>
+	public static class Logger
+	{
+		#region Public Methods
+
+		public static void Debug(Type callerType, string message, Exception exception = null)
+		{
+			LogManager.GetLogger(callerType).Debug(message, exception);
+		}
+
+		public static void Error(Type callerType, string message, Exception exception = null)
+		{
+			LogManager.GetLogger(callerType).Error(message, exception);
+		}
+
+		public static void Fatal(Type callerType, string message, Exception exception = null)
+		{
+			LogManager.GetLogger(callerType).Fatal(message, exception);
+		}
+
+		public static void Info(Type callerType, string message, Exception exception = null)
+		{
+			LogManager.GetLogger(callerType).Info(message, exception);
+		}
+
+		public static void Warn(Type callerType, string message, Exception exception = null)
+		{
+			LogManager.GetLogger(callerType).Warn(message, exception);
+		}
+
+		#endregion Public Methods
+	}
+}
