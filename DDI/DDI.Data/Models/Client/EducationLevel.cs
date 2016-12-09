845906d675f9868230ec0ac9fe88461c094dbf29
Joshua Terry <jterry@ddi.org>
2016-12-09 15:58:02 -0500

    DC-63 : DC-106 : DC-101 - Added Migration Script for changes.  Changed spacing in Education.

--- DDI/DDI.Data/Models/Client/EducationLevel.cs
+++ DDI/DDI.Data/Models/Client/EducationLevel.cs
@@ -10,18 +10,13 @@ namespace DDI.Data.Models.Client
 	public class EducationLevel
 	{
 		#region Public Properties
-
 		public string Code { get; set; }
-
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public Guid Id { get; set; }
-
 		public bool IsActive { get; set; }
-
 		public string Name { get; set; }
         public virtual Education Education { get; set; }
-
         public Guid? EducationId { get; set; }
         #endregion Public Properties
     }
