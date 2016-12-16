7f4d96f5aa16e657629a0e6e62cb577e0085c451
DDI\jallen <jallen@ddi.org>
2016-12-16 16:28:54 -0500

    DC-141: Reverse changes to startup.cs

--- DDI/DDI.Business/Startup.cs
+++ DDI/DDI.Business/Startup.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using DDI.Business.Domain;
 using Microsoft.Owin;
 using Owin;
 
@@ -14,17 +13,6 @@ namespace DDI.Business
         public void Configuration(IAppBuilder app)
         {
             ConfigureAuth(app);
-            Test();
-        }
-
-        public void Test()
-        {
-            var domain = new ConstituentDomain();
-            var name = domain.Repository.Entities.FirstOrDefault(p => p.LastName == "Byers");
-            name.FirstName = "Elizabeth";
-
-            domain.Validate(name);
-
         }
     }
 }
