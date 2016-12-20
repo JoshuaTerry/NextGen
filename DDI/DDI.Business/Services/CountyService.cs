d2b86c746cd9e3f745c29b927f1dbb386df6ef66
Ryan Clough <w.ryan.clough@gmail.com>
2016-12-20 15:47:56 -0500

    DC-1: changed orderby for CountyService  Changes to be committed: 	modified:   DDI/DDI.Business/Services/CountyService.cs

--- DDI/DDI.Business/Services/CountyService.cs
+++ DDI/DDI.Business/Services/CountyService.cs
@@ -18,11 +18,10 @@ namespace DDI.Business.Services
         {
             var result = Repository.Entities;
             var query = new CriteriaQuery<County, CountySearch>(result, search)
-                .IfModelPropertyIsNotBlankAndItEqualsDatabaseField(m => m.StateId, d => d.StateId)
-                .SetOrderBy(search?.OrderBy);
+                .IfModelPropertyIsNotBlankAndItEqualsDatabaseField(m => m.StateId, d => d.StateId);
 
             //var sql = query.GetQueryable().ToString();  //This shows the SQL that is generated
-            return GetIDataResponse(() => query.GetQueryable().ToList());
+            return GetIDataResponse(() => query.GetQueryable().ToList().OrderBy(c => c.DisplayName).ToList());
         }
 
         #endregion Public Methods
