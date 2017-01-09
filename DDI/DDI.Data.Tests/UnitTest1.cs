58f051e6e85e1b08283eda6c6083970ed446048f
DDI\jallen <jallen@ddi.org>
2017-01-09 09:31:34 -0500

    DC-153: Rollback changes to DDI.Data.Tests project.

--- DDI/DDI.Data.Tests/UnitTest1.cs
+++ DDI/DDI.Data.Tests/UnitTest1.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-
-using DDI.Data;
-using DDI.Data.Models.Client;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace DDI.Data.Tests
@@ -9,24 +6,9 @@ namespace DDI.Data.Tests
     [TestClass]
     public class UnitTest1
     {
-        #region Methods
-
         [TestMethod]
         public void TestMethod1()
         {
-            using (var context = new DomainContext())
-            {
-                Guid id = Guid.Parse("29F025C3-0CC3-E611-80E3-005056B7555A");
-                var repository = new Repository<Constituent>(context);
-                var constituent =repository.GetById(
-                    id, 
-                    c => c.ConstituentAddresses, 
-                    c => c.Gender);
-
-                Console.WriteLine(constituent);
-            }
         }
-
-        #endregion Methods
     }
 }
