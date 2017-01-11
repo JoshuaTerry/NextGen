00b1c4b6c4ec70cd96330b40a8bc69667af48080
DDI\jallen <jallen@ddi.org>
2017-01-11 10:42:36 -0500

    DC-221: DC-257: ReflectionHelper tests.

---
+++ DDI/DDI.Shared.Test/Helpers/ReflectionHelperTests.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using DDI.Shared.Helpers;
+using DDI.Shared.ModuleInfo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DDI.Shared.Test.Helpers
+{
+    [TestClass]
+    public class ReflectionHelperTests
+    {
+        private const string TESTDESCR = "Shared | Helpers";
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void ReflectionHelper_FindTypeByName()
+        {
+            // Note - this test also tests ReflectionHelper.GetLocalAssemblies.
+
+            Assert.AreEqual(typeof(DDI.Shared.Logger.Logger), ReflectionHelper.FindTypeByName(typeof(DDI.Shared.Logger.Logger).FullName),
+                "FindTypeByName finds type by scanning assemblies.");
+            Assert.AreEqual(typeof(DDI.Shared.Logger.Logger), ReflectionHelper.FindTypeByName(typeof(DDI.Shared.Logger.Logger).FullName,
+                typeof(DDI.Shared.Logger.Logger).Assembly), "FindTypeByName finds type by scanning assemblies.");
+        }
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void ReflectionHelper_GetDecoratedWith()
+        {
+            var types = ReflectionHelper.GetDecoratedWith<ModuleTypeAttribute>();
+            Assert.IsNotNull(types, "GetDecoratedWith returned non-null value.");
+            Assert.IsTrue(types.Count > 10, "GetDecoratedWith returned multiple results.");
+            CollectionAssert.Contains(types, typeof(ModuleInfo.Accounting), "GetDecoratedWith contains Accounting module.");
+        }
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void ReflectionHelper_GetDerivedTypes()
+        {
+            var types = ReflectionHelper.GetDerivedTypes<ModuleInfo.Base.ModuleInfoBase>();
+            Assert.IsNotNull(types, "GetDerivedTypes returned non-null value.");
+            Assert.IsTrue(types.Count > 10, "GetDerivedTypes returned multiple results.");
+            CollectionAssert.Contains(types, typeof(ModuleInfo.Accounting), "GetDerivedTypes contains Accounting module.");
+        }
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void ReflectionHelper_GetImplementingTypes()
+        {
+            var types = ReflectionHelper.GetImplementingTypes<IDoesSomething>();
+            Assert.IsNotNull(types, "GetImplementingTypes returned non-null value.");
+            Assert.IsTrue(types.Count == 2, "GetImplementingTypes returned multiple results.");
+            CollectionAssert.Contains(types, typeof(MyClass), "GetImplementingTypes contains MyClass.");
+            CollectionAssert.Contains(types, typeof(MySubClass), "GetImplementingTypes contains MySubClass.");
+        }
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void ReflectionHelper_GetEnums()
+        {
+            var types = ReflectionHelper.GetEnums(typeof(DDI.Shared.Logger.Logger).Assembly).ToList();
+            Assert.IsNotNull(types, "GetEnums returned non-null value.");           
+            CollectionAssert.Contains(types, typeof(DDI.Shared.Enums.ModuleType), "GetEnums contains ModuleType enumeration.");
+        }
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void ReflectionHelper_GetInstancesOf()
+        {
+            var instances = ReflectionHelper.GetInstancesOf<IDoesSomething>(typeof(ReflectionHelperTests).Assembly);
+            Assert.IsNotNull(instances, "GetInstancesOf returned non-null value.");
+            Assert.IsTrue(instances.Count == 2, "GetInstancesOf returned multiple results.");
+            CollectionAssert.AllItemsAreInstancesOfType(instances, typeof(IDoesSomething), "GetInstancesOf returned instances of IDoesSomething.");
+        }        
+
+        // Interfaces and classes used for these tests.
+        private interface IDoesSomething
+        {
+        }
+
+        private class MyClass : IDoesSomething { }
+        private class MySubClass : MyClass { }
+    }
+}
--- DDI/DDI.Shared/Helpers/ModuleHelper.cs
+++ DDI/DDI.Shared/Helpers/ModuleHelper.cs
@@ -20,45 +20,7 @@ namespace DDI.Shared.Helpers
 
         #endregion
 
-        #region Constructors
-
-        static ModuleHelper()
-        {
-            _modules = new List<ModuleInfoBase>();
-            _moduleDictionary = new Dictionary<ModuleType, ModuleInfoBase>();
-
-            // Populate the list of modules and the module dictionary.
-            foreach (Type type in ReflectionHelper.GetDerivedTypes<ModuleInfoBase>(typeof(ModuleHelper).Assembly))
-            {
-                ModuleInfoBase moduleInfo = (ModuleInfoBase)Activator.CreateInstance(type);
-                _modules.Add(moduleInfo);
-                ModuleType moduleType = moduleInfo.ModuleType;
-                if (moduleType != ModuleType.None)
-                {
-                    _moduleDictionary[moduleType] = moduleInfo;
-                }
-            }            
-
-            // Link up modules
-            foreach (var module in _modules)
-            {
-                ModuleType parent = module.ParentModuleType;
-                if (parent != ModuleType.None)
-                {
-                    ModuleInfoBase parentModule = _modules.FirstOrDefault(p => p.ModuleType == parent);
-                    if (parentModule != null)
-                    {
-                        module.ParentModule = parentModule;
-                        parentModule.ChildModules.Add(module);
-                    }
-                }
-            }
-
-        }
-
-        #endregion
-
-        #region Properties
+        #region Public Properties
 
         /// <summary>
         /// Collection of module information objects
@@ -67,19 +29,21 @@ namespace DDI.Shared.Helpers
         {
             get
             {
+                Initialize();
                 return _modules;
             }
         }
 
         #endregion
 
-        #region Methods
+        #region Public Methods
 
         /// <summary>
         /// Get the ModuleInfo object for a specified ModuleType value.
         /// </summary>
         public static ModuleInfoBase GetModuleInfo (ModuleType moduleType)
         {
+            Initialize();
             ModuleInfoBase moduleInfo = null;
             _moduleDictionary.TryGetValue(moduleType, out moduleInfo);
 
@@ -92,10 +56,56 @@ namespace DDI.Shared.Helpers
         /// <typeparam name="T">ModuleInfo type</typeparam>
         public static T GetModuleInfo<T>() where T : ModuleInfoBase
         {
+            Initialize();
             return _modules.FirstOrDefault(p => p.GetType() == typeof(T)) as T;
         }
 
         #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Build list of modules via reflection only once, and only when first requested.
+        /// </summary>
+        private static void Initialize()
+        {
+            if (_modules == null)
+            {
+                _modules = new List<ModuleInfoBase>();
+                _moduleDictionary = new Dictionary<ModuleType, ModuleInfoBase>();
+
+                // Populate the list of modules and the module dictionary.
+                foreach (Type type in ReflectionHelper.GetDerivedTypes<ModuleInfoBase>(typeof(ModuleHelper).Assembly))
+                {
+                    ModuleInfoBase moduleInfo = (ModuleInfoBase)Activator.CreateInstance(type);
+                    _modules.Add(moduleInfo);
+                    ModuleType moduleType = moduleInfo.ModuleType;
+                    if (moduleType != ModuleType.None)
+                    {
+                        _moduleDictionary[moduleType] = moduleInfo;
+                    }
+                }
+
+                // Link up modules
+                foreach (var module in _modules)
+                {
+                    ModuleType parent = module.ParentModuleType;
+                    if (parent != ModuleType.None)
+                    {
+                        ModuleInfoBase parentModule = _modules.FirstOrDefault(p => p.ModuleType == parent);
+                        if (parentModule != null)
+                        {
+                            module.ParentModule = parentModule;
+                            parentModule.ChildModules.Add(module);
+                        }
+                    }
+                }
+            }
+        }
+
+        #endregion
+
+
 
 
     }
