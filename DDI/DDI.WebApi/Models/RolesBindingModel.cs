a02d07d41c844a9ae75d047d664fddbc0c470fcd
Ryan Clough <w.ryan.clough@gmail.com>
2017-01-12 13:58:08 -0500

    DC-224: refactored newly added RolesController

---
+++ DDI/DDI.WebApi/Models/RolesBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DDI.WebApi.Models
+{
+    public class RolesBindingModel
+    {
+        [Required]
+        [Display(Name = "Roles")]
+        public string[] Roles { get; set; }
+    }
+}
--- DDI/DDI.WebApi/Models/RolesController.cs
+++ DDI/DDI.WebApi/Models/RolesController.cs
@@ -5,13 +5,17 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Results;
+using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
 using Microsoft.AspNet.Identity.Owin;
+using WebGrease.Css.Extensions;
 
 namespace DDI.WebApi.Models
 {
     public class RolesController : ApiController
     {
+        private ApplicationUserManager _userManager;
         private ApplicationRoleManager _roleManager;
 
         public RolesController()
@@ -19,11 +23,12 @@ namespace DDI.WebApi.Models
             
         }
 
-        public RolesController(ApplicationRoleManager roleManager)
+        public RolesController(ApplicationUserManager userManager, ApplicationRoleManager roleManager)
         {
+            UserManager = userManager;
             RoleManager = roleManager;
         }
-
+        
         public ApplicationRoleManager RoleManager
         {
             get
@@ -36,19 +41,32 @@ namespace DDI.WebApi.Models
             }
         }
 
-        [HttpPost]
-        [Route("api/v1/Roles/Add")]
-        public async Task<IHttpActionResult> Add(string[] roles)
+        public ApplicationUserManager UserManager
         {
-            if (CanRolesBeAdded(roles) != null)
+            get
             {
-                ModelState.AddModelError("", CanRolesBeAdded(roles));
+                return _userManager ?? Request.GetOwinContext().GetUserManager<ApplicationUserManager>();
+            }
+            private set
+            {
+                _userManager = value;
+            }
+        }
+
+
+        [HttpPost]
+        [Route("api/v1/roles/add")]
+        public async Task<IHttpActionResult> Add(RolesBindingModel model)
+        {
+            if (CanRolesBeAdded(model.Roles) != null)
+            {
+                ModelState.AddModelError("", CanRolesBeAdded(model.Roles));
                 return BadRequest(ModelState);
             }
 
             try
             {
-                foreach (var role in roles)
+                foreach (var role in model.Roles)
                 {
                     await RoleManager.CreateAsync(new IdentityRole(role));
                 }
@@ -61,19 +79,56 @@ namespace DDI.WebApi.Models
             return Ok();
         }
 
-        [HttpPost]
-        [Route("api/v1/Roles/Add")]
-        public async Task<IHttpActionResult> Remove(string[] roles)
+        [HttpGet]
+        [Route("api/v1/roles")]
+        public IHttpActionResult Get()
         {
-            if (CanRolesBeRemoved(roles) != null)
+            return Ok(RoleManager.Roles.ToList());
+        }
+
+        [HttpPost]
+        [Route("api/v1/roles/update")]
+        public async Task<IHttpActionResult> Update(string role, string newRoleName)
+        {
+            var roleToUpdate = RoleManager.FindByIdAsync(role).Result;
+            if (roleToUpdate == null)
             {
-                ModelState.AddModelError("", CanRolesBeRemoved(roles));
+                ModelState.AddModelError("", $"Role {roleToUpdate.Name} not found.");
+                return BadRequest(ModelState);
+            }
+
+            if (string.IsNullOrWhiteSpace(newRoleName))
+            {
+                ModelState.AddModelError("", $"You must supply a new name for role {roleToUpdate.Name}");
                 return BadRequest(ModelState);
             }
 
             try
             {
-                foreach (var role in roles)
+                roleToUpdate.Name = newRoleName;
+                await RoleManager.UpdateAsync(roleToUpdate);
+            }
+            catch (Exception)
+            {
+                return InternalServerError();
+            }
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("api/v1/roles/remove")]
+        public async Task<IHttpActionResult> Delete([FromBody] RolesBindingModel model)
+        {
+            if (CanRolesBeRemoved(model.Roles) != null)
+            {
+                ModelState.AddModelError("", CanRolesBeRemoved(model.Roles));
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                foreach (var role in model.Roles)
                 {
                     var roleToDelete = RoleManager.FindByNameAsync(role).Result;
                     if (roleToDelete != null)
@@ -93,12 +148,31 @@ namespace DDI.WebApi.Models
 
         private string CanRolesBeAdded(string[] roles)
         {
-            return roles.Where(role => RoleManager.Roles.Any(r => r.Name == role)).Aggregate<string, string>(null, (current, role) => current + $"Role {role} has already been created.\n");
+            return roles.Where(role => RoleManager.RoleExists(role)).Aggregate<string, string>(null, (current, role) => current + $"Role {role} has already been created.\n");
         }
 
         private string CanRolesBeRemoved(string[] roles)
         {
-            return roles.Where(role => !RoleManager.Roles.Any(r => r.Name == role)).Aggregate<string, string>(null, (current, role) => current + $"Role {role} does not exist.\n");
+            string errorMessage = null;
+
+            foreach (var role in roles)
+            {
+                var roleToCheck = RoleManager.FindByName(role);
+                if (roleToCheck != null)
+                {
+                    var usersInRole = roleToCheck.Users.Count;
+                    if (usersInRole > 0)
+                    {
+                        errorMessage += $"Role {role} has {usersInRole} users associated.\n";
+                    }
+                }
+                else
+                {
+                    errorMessage += $"Role {role} was not found.\n";
+                }
+            }
+
+            return errorMessage;
         }
     }
 }
