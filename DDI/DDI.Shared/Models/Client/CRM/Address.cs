4234ee969701cd9738fd117469db94d884f145ae
DDI\jallen <jallen@ddi.org>
2017-01-12 20:20:45 -0500

    DC-277: Remove LInkedEntity logic and replace with helper class.

--- DDI/DDI.Shared/Models/Client/CRM/Address.cs
+++ DDI/DDI.Shared/Models/Client/CRM/Address.cs
@@ -4,11 +4,10 @@ using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using DDI.Shared.Models.Common;
-using DDI.Shared.Attributes;
 
 namespace DDI.Shared.Models.Client.CRM
 {
-    [Table("Address"), EntityType("CRM_Address")]
+    [Table("Address")]
     public class Address : EntityBase
     {
         #region Public Properties
