187ee607eb83d51c1affff3bb6ed2c659933d03e
jontrick2001 <jtrick@ddi.org>
2017-01-17 15:28:47 -0500

    DC-157: Merge conflict resolutions

--- DDI/DDI.Shared/Models/Client/CRM/Constituent.cs
+++ DDI/DDI.Shared/Models/Client/CRM/Constituent.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
+using DDI.Shared.Attributes;
+using DDI.Shared.Enums.CRM;
+using DDI.Shared.Models.Client.Core;
 using DDI.Shared.Statics;
 
 namespace DDI.Shared.Models.Client.CRM
