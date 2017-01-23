53289be792fbe4f5813d4ff874603607b236a9d6
Joshua Terry <jterry@ddi.org>
2017-01-23 16:39:26 -0500

    DC-223 Location Info Stuff.

---
+++ DDI/DDI.WebApi/Controllers/LocationsController.cs
@@ -0,0 +1,37 @@
+﻿using DDI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace DDI.WebApi.Controllers
+{
+    public class LocationsController : ApiController
+    {
+        ILocationService _service;
+        public LocationsController() : this(new LocationService()) { }
+
+        public LocationsController(ILocationService service)
+        {
+            this._service = service;
+        }
+        
+        [HttpGet]
+        [Route("api/v1/locations")]
+        public IHttpActionResult RefineLocation(string addressLine1, string addressLine2, string city, Guid? countryId, Guid? countyId, Guid? stateId, string zip)
+        {
+            var result = _service.RefineAddress(addressLine1, addressLine2, city, countryId, countyId, stateId, zip);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            if (!result.IsSuccessful)
+            {
+                return InternalServerError();
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ DDI/Services/LocationService.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using DDI.Business.Common;
+using DDI.Shared;
+using DDI.Data;
+using DDI.Shared.Models.Common;
+
+namespace DDI.Services
+{
+    public class LocationService : ILocationService
+    {
+        private IUnitOfWork _unitOfWork;
+        public LocationService()
+        {
+            Initialize(new UnitOfWorkEF());
+        }
+        public LocationService(IUnitOfWork uow)
+        {
+            Initialize(uow);
+        }
+        private void Initialize(IUnitOfWork uow)
+        {
+            _unitOfWork = uow; 
+        }
+        public IDataResponse<ZipLookupInfo> RefineAddress(string addressLine1, string addressLine2, string city, Guid? countryId, Guid? countyId, Guid? stateId, string zip)
+        {
+            Country country = null;
+            County county = null;
+            State state = null;
+
+            if (countryId != null)
+                country = this._unitOfWork.GetRepository<Country>().Entities.FirstOrDefault(c => c.Id == countryId);
+
+            if (countyId != null)
+                county = this._unitOfWork.GetRepository<County>().Entities.FirstOrDefault(c => c.Id == countyId);
+
+            if (stateId != null)
+                state = this._unitOfWork.GetRepository<State>().Entities.FirstOrDefault(s => s.Id == stateId);
+
+            var address = new ZipLookupInfo();
+            address.AddressLine1 = addressLine1;
+            address.AddressLine2 = addressLine2;
+            address.City = city;
+            address.PostalCode = zip;
+
+            if (country != null)
+                address.Country = country;
+
+            if (county != null)
+                address.County = county;
+
+            if (state != null)
+                address.State = state;
+
+            string resultAddress = string.Empty;
+            ZipLookup zl = new ZipLookup();
+            zl.Zip4Lookup(address, out resultAddress);
+
+            IDataResponse<ZipLookupInfo> response = new DataResponse<ZipLookupInfo>();
+            response.Data = address;
+            return response;
+        }
+    }
+}
