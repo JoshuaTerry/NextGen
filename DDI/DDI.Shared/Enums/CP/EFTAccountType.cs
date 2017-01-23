5b02c6bdc198aeeeecb566b5d672fc59620834f7
DDI\jallen <jallen@ddi.org>
2017-01-23 18:17:42 -0500

    DC-320: Data models for payment methods, constituent payment method.

---
+++ DDI/DDI.Shared/Enums/CP/EFTAccountType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace DDI.Shared.Enums.CP
+{
+    public enum EFTAccountType
+    {
+        Checking = 0,
+        Savings = 1
+    }
+
+}
--- DDI/DDI.Shared/Models/Client/CRM/Constituent.cs
+++ DDI/DDI.Shared/Models/Client/CRM/Constituent.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using DDI.Shared.Attributes;
 using DDI.Shared.Enums.CRM;
 using DDI.Shared.Models.Client.Core;
+using DDI.Shared.Models.Client.CP;
 using DDI.Shared.Statics;
 
 namespace DDI.Shared.Models.Client.CRM
@@ -110,6 +111,8 @@ namespace DDI.Shared.Models.Client.CRM
         [Column(TypeName = "date")]
         public DateTime? OrdinationDate { get; set; }
 
+        public PaymentMethod PreferredPaymentMethod { get; set; }
+
         [MaxLength(128)]
         public string PlaceOfOrdination { get; set; }
 
@@ -166,6 +169,8 @@ namespace DDI.Shared.Models.Client.CRM
         public ICollection<Ethnicity> Ethnicities { get; set; }
         public ICollection<PaymentPreference> PaymentPreferences { get; set; }
         public ICollection<Tag> Tags { get; set; }
+
+        public ICollection<PaymentMethodBase> PaymentMethods { get; set; }
 
         [InverseProperty(nameof(Relationship.Constituent1))]
         public ICollection<Relationship> Relationship1s { get; set; }
