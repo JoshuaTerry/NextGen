c1a76a5c8264a0accb95b961734708dc29e622c6
jontrick2001 <jtrick@ddi.org>
2017-02-09 17:12:52 -0500

    DC-380: Add routes for relationships

--- DDI/DDI.Shared/Models/Client/CRM/Constituent.cs
+++ DDI/DDI.Shared/Models/Client/CRM/Constituent.cs
@@ -185,6 +185,7 @@ namespace DDI.Shared.Models.Client.CRM
         [HateoasCollectionLink(RouteNames.PaymentPreference + RouteNames.EFT)]
         public ICollection<EFTPaymentMethod> EFTPaymentMethods { get; set; }
 
+        [HateoasCollectionLink(RouteNames.Relationship)] //both relationships are handled with the same route
         [InverseProperty(nameof(Relationship.Constituent1))]
         public ICollection<Relationship> Relationship1s { get; set; }
 
---
+++ DDI/DDI.WebApi/Controllers/RelationshipTypesController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using DDI.Shared.Models.Client.CRM;
+using DDI.Services;
+using DDI.Services.Search;
+using DDI.Shared.Statics;
+
+namespace DDI.WebApi.Controllers
+{
+    public class RelationshipTypesController : ControllerBase<RelationshipType>
+    {
+        [HttpGet]
+        [Route("api/v1/relationshiptypes", Name = RouteNames.RelationshipTypes)]
+        public IHttpActionResult GetAll(int? limit = SearchParameters.LimitMax, int? offset = SearchParameters.OffsetDefault, string orderBy = OrderByProperties.DisplayName, string fields = null)
+        {
+            return base.GetAll(RouteNames.RelationshipTypes, limit, offset, orderBy, fields);
+        }
+    }
+}
