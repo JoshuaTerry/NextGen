3f6bb8f81b02efecdc5b66b1a36efbd5b8121fa6
DDI\mreynolds <mreynolds@ddi.org>
2017-02-22 12:35:02 -0500

    Fixed login / authentication issue with CORS

--- DDI/DDI.WebApi/Startup.cs
+++ DDI/DDI.WebApi/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using DDI.WebApi;
-using Microsoft.Owin;
 using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+
 
 [assembly: OwinStartup(typeof(Startup))]
 
@@ -10,6 +12,8 @@ namespace DDI.WebApi
     {
         public void Configuration(IAppBuilder app)
         {
+            app.UseCors(CorsOptions.AllowAll);
+
             ConfigureAuth(app);
 
             // Set the CacheHelper's cache provider
