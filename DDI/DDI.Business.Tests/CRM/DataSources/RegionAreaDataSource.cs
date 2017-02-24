e31d3e0a47a7c0111fe69453d4573e6f24a89338
DDI\jallen <jallen@ddi.org>
2017-02-24 16:49:51 -0500

    Cleanup unit tests. Get RegionLogic tests working.

---
+++ DDI/DDI.Business.Tests/CRM/DataSources/RegionAreaDataSource.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DDI.Business.Tests.Common.DataSources;
+using DDI.Business.Tests.Helpers;
+using DDI.Data;
+using DDI.Shared.Models.Client.CRM;
+using DDI.Shared.Statics.CRM;
+
+namespace DDI.Business.Tests.CRM.DataSources
+{
+    public static class RegionAreaDataSource
+    {
+        public static IList<RegionArea> GetDataSource(UnitOfWorkNoDb uow)
+        {
+            IList<RegionArea> existing = uow.GetRepositoryOrNull<RegionArea>()?.Entities.ToList();
+            if (existing != null)
+            {
+                return existing;
+            }
+
+            var regions = RegionDataSource.GetDataSource(uow);
+            var states = StateDataSource.GetDataSource(uow);
+            var counties = CountyDataSource.GetDataSource(uow);
+
+            var indiana = states.FirstOrDefault(p => p.Country.ISOCode == AddressDefaults.DefaultCountryCode && p.StateCode == "IN");
+            var ohio = states.FirstOrDefault(p => p.Country.ISOCode == AddressDefaults.DefaultCountryCode && p.StateCode == "OH");
+
+            var areas = new List<RegionArea>();
+
+            areas.Add(new RegionArea()
+            {
+                Region = regions.FirstOrDefault(p => p.Level == 1 && p.Code == "IN"),
+                State = indiana,
+                Priority = 1,
+                Level = 1,
+                Id = GuidHelper.NextGuid()
+            });
+
+            areas.Add(new RegionArea()
+            {
+                Region = regions.FirstOrDefault(p => p.Level == 1 && p.Code == "OH"),
+                Country = ohio.Country,
+                State = ohio,
+                Priority = 1,
+                Level = 1,
+                Id = GuidHelper.NextGuid()
+            });
+
+            areas.Add(new RegionArea()
+            {
+                Region = regions.FirstOrDefault(p => p.Level == 2 && p.Code == "INDY"),
+                Country = indiana.Country,
+                PostalCodeLow = "46201",
+                PostalCodeHigh = "46299",
+                Priority = 1,
+                Level = 2,
+                Id = GuidHelper.NextGuid()
+            });
+
+            areas.Add(new RegionArea()
+            {
+                Region = regions.FirstOrDefault(p => p.Level == 2 && p.Code == "HAML"),
+                Country = indiana.Country,
+                County = indiana.Counties.FirstOrDefault(p => p.Description == "Hamilton"),
+                State = indiana,
+                Priority = 1,
+                Level = 2,
+                Id = GuidHelper.NextGuid()
+            });
+
+            areas.Add(new RegionArea()
+            {
+                Region = regions.FirstOrDefault(p => p.Level == 2 && p.Code == "FISH"),
+                Country = indiana.Country,
+                County = indiana.Counties.FirstOrDefault(p => p.Description == "Hamilton"),
+                State = indiana,
+                City = "Fishers",
+                Priority = 2,
+                Level = 2,
+                Id = GuidHelper.NextGuid()
+            });
+
+            uow.CreateRepositoryForDataSource(areas);
+            return areas;
+        }    
+
+    }
+
+    
+}
