03a74c5df6f0c44747ec29b71940e086b6e2e26e
DDI\jterry <jterry@ddi.org>
2017-02-24 15:38:10 -0500

    DC-377

---
+++ DDI/DDI.Shared/Models/Client/Core/UserLogin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DDI.Shared.Models.Client.Core
+{
+    [Table("AspNetUsers")]
+    public class UserLogin : EntityBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public override Guid Id { get; set; }
+        [MaxLength(256)]
+        public string Email { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnable { get; set; }
+        public DateTime? LockoutEndDateUtc { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
+        public DDIUser DDIUser { get; set; }
+        public Guid DDIUserId { get; set; }
+        [MaxLength(256)]
+        public string UserName { get; set; }
+        [NotMapped]
+        public override Guid? CreatedBy { get; set; }
+        [NotMapped]
+        public override DateTime? CreatedOn { get; set; }
+        [NotMapped]
+        public override Guid? LastModifiedBy { get; set; }
+        [NotMapped]
+        public override DateTime? LastModifiedOn { get; set; }
+        public override string DisplayName
+        {
+            get
+            {
+                return UserName;
+            }
+        }
+    }
+}
