e0dc10a909cf0c379af2b6690e707eb09e7e1c78
DDI\jterry <jterry@ddi.org>
2017-02-24 11:20:24 -0500

    EFAuditLogging : DC-377 new models

--- DDI/DDI.Shared/Models/Client/Core/DDIUser.cs
+++ DDI/DDI.Shared/Models/Client/Core/DDIUser.cs
@@ -13,6 +13,26 @@ namespace DDI.Shared.Models.Client.Core
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public override Guid Id { get; set; }
-        public string Name { get; set; }
+        [MaxLength(256)]
+        public string FirstName { get; set; }
+        [MaxLength(256)]
+        public string MiddleName { get; set; }
+        [MaxLength(256)]
+        public string LastName { get; set; }
+        [MaxLength(256)]
+        public string EmailAddress { get; set; }
+        public Guid? UserLoginId { get; set; }
+        public UserLogin UserLogin { get; set; }
+        public override Guid? CreatedBy { get; set; } 
+        public override DateTime? CreatedOn { get; set; } 
+        public override Guid? LastModifiedBy { get; set; } 
+        public override DateTime? LastModifiedOn { get; set; }
+        public override string DisplayName
+        {
+            get
+            {
+                return $"{FirstName} {LastName}";
+            }
+        }
     }
 }
--- DDI/Services/AuditService.cs
+++ DDI/Services/AuditService.cs
@@ -4,6 +4,7 @@ using DDI.Shared.Models.Client.Audit;
 using DDI.Shared.Models.Client.Core;
 using System;
 using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
@@ -22,36 +23,90 @@ namespace DDI.Services
             this._uow = uow;
         }
 
-        public void GetAllWhereExpression(Expression<Func<ChangeSet, bool>> expression, IPageable search = null)
+        // JLT - Can't find a way when calling to refer to the sub-types on ChangeSet in the expression
+        public IDataResponse<List<dynamic>> GetAllWhereExpression(Expression<Func<ChangeSet, bool>> expression, IPageable search = null)
         {
-            var a = _uow.GetRepository<ChangeSet>().Entities
-                             .Where(expression)
-                             .Join(_uow.GetRepository<ObjectChange>().Entities,
-                                     cs => cs.Id,
-                                     oc => oc.ChangeSetId,
-                                     (cs, oc) => new { cs, oc })
-                             .GroupJoin(_uow.GetRepository<PropertyChange>().Entities,
-                                     outer => outer.oc.Id,
-                                     pc => pc.ObjectChangeId,
-                                     (outer, pc) => new { cs = outer.cs, oc = outer.oc, pc })
-                             .SelectMany(x => x.pc.DefaultIfEmpty(),
-                                     (outer, pc) => new { cs = outer.cs, oc = outer.oc, pc })
-                             .Join(_uow.GetRepository<DDIUser>().Entities,
-                                     outer => outer.cs.UserId,
-                                     u => u.Id,
-                                     (outer, u) => new
-                                     {
-                                         ChangeSetId = outer.cs.Id,
-                                         Timestamp = outer.cs.Timestamp,
-                                         User = u.DisplayName,
-                                         EntityType = outer.oc.TypeName,
-                                         EntityValue = outer.oc.DisplayName,
-                                         Property = outer.pc.PropertyName,
-                                         OldValue = outer.pc.OriginalValue,
-                                         NewValue = outer.pc.Value
-                                     });
- 
-            //return GetPagedResults(queryable, search);
+            var response = new DataResponse<List<dynamic>>();
+            try
+            {
+                var results = _uow.GetRepository<ChangeSet>().Entities
+                                 .Where(expression)
+                                 .Join(_uow.GetRepository<ObjectChange>().Entities,
+                                         cs => cs.Id,
+                                         oc => oc.ChangeSetId,
+                                         (cs, oc) => new { cs, oc })
+                                 .GroupJoin(_uow.GetRepository<PropertyChange>().Entities,
+                                         outer => outer.oc.Id,
+                                         pc => pc.ObjectChangeId,
+                                         (outer, pc) => new { cs = outer.cs, oc = outer.oc, pc })
+                                 .SelectMany(x => x.pc.DefaultIfEmpty(),
+                                         (outer, pc) => new { cs = outer.cs, oc = outer.oc, pc })
+                                 .Join(_uow.GetRepository<DDIUser>().Entities,
+                                         outer => outer.cs.UserId,
+                                         u => u.Id,
+                                         (outer, u) => new
+                                         {
+                                             ChangeSetId = outer.cs.Id,
+                                             Timestamp = outer.cs.Timestamp,
+                                             User = u.DisplayName,
+                                             EntityType = outer.oc.TypeName,
+                                             EntityValue = outer.oc.DisplayName,
+                                             Property = outer.pc.PropertyName,
+                                             OldValue = outer.pc.OriginalValue,
+                                             NewValue = outer.pc.Value
+                                         }).ToList<dynamic>();
+
+                response.Data = results;
+            }
+            catch (Exception ex)
+            {
+                response.IsSuccessful = false;
+                response.ErrorMessages.Add(ex.Message);
+            }
+
+            return response;
         }
+        public IDataResponse<List<dynamic>> GetAll(Guid id, DateTime start, DateTime end, IPageable search = null)
+        {
+            var response = new DataResponse<List<dynamic>>();
+            try
+            {
+                var results = _uow.GetRepository<ChangeSet>().Entities
+                                 .Where(c => c.Timestamp >= start && c.Timestamp <= end)
+                                 .Join(_uow.GetRepository<ObjectChange>().Entities.Where(o => o.ObjectReference == id.ToString()),
+                                         cs => cs.Id,
+                                         oc => oc.ChangeSetId,
+                                         (cs, oc) => new { cs, oc })
+                                 .GroupJoin(_uow.GetRepository<PropertyChange>().Entities,
+                                         outer => outer.oc.Id,
+                                         pc => pc.ObjectChangeId,
+                                         (outer, pc) => new { cs = outer.cs, oc = outer.oc, pc })
+                                 .SelectMany(x => x.pc.DefaultIfEmpty(),
+                                         (outer, pc) => new { cs = outer.cs, oc = outer.oc, pc })
+                                 .Join(_uow.GetRepository<DDIUser>().Entities,
+                                         outer => outer.cs.UserId,
+                                         u => u.Id,
+                                         (outer, u) => new
+                                         {
+                                             ChangeSetId = outer.cs.Id,
+                                             Timestamp = outer.cs.Timestamp,
+                                             User = u.DisplayName,
+                                             EntityType = outer.oc.TypeName,
+                                             EntityValue = outer.oc.DisplayName,
+                                             Property = outer.pc.PropertyName,
+                                             OldValue = outer.pc.OriginalValue,
+                                             NewValue = outer.pc.Value
+                                         }).ToList<dynamic>();
+
+                response.Data = results;
+            }
+            catch(Exception ex)
+            {
+                response.IsSuccessful = false;
+                response.ErrorMessages.Add(ex.Message);
+            }
+
+            return response;
+        }       
     }
 }
