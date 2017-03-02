01c14a09d3f73f8971b261d0de535e23e0f360f5
DDI\sbansode <sbansode@ddi.org>
2017-03-02 12:08:38 -0500

    DC-482 DC-482 Added Profession and Income Level grids in Professional section

--- DDI/DDI.WebApi/Controllers/ProfessionsController.cs
+++ DDI/DDI.WebApi/Controllers/ProfessionsController.cs
@@ -1,9 +1,11 @@
-﻿using DDI.Shared.Models.Client.CRM;
-using DDI.Services;
+﻿using System;
+using System.Drawing;
 using System.Web.Http;
-using System.Web.Mvc.Html;
+using DDI.Shared.Models.Client.CRM;
+using DDI.Services;
 using DDI.Services.Search;
 using DDI.Shared.Statics;
+using Newtonsoft.Json.Linq;
 
 namespace DDI.WebApi.Controllers
 {
@@ -14,6 +16,34 @@ namespace DDI.WebApi.Controllers
         public IHttpActionResult GetAll(int? limit = SearchParameters.LimitMax, int? offset = SearchParameters.OffsetDefault, string orderBy = OrderByProperties.DisplayName, string fields = null)
         {
             return base.GetAll(RouteNames.Profession, limit, offset, orderBy, fields);
+        }
+
+        [HttpGet]
+        [Route("api/v1/professions/{id}", Name = RouteNames.Profession + RouteVerbs.Get)]
+        public IHttpActionResult GetById(Guid id, string fields = null)
+        {
+            return base.GetById(id, fields);
+        }
+
+        [HttpPost]
+        [Route("api/v1/professions", Name = RouteNames.Profession + RouteVerbs.Post)]
+        public IHttpActionResult Post([FromBody] Profession entityToSave)
+        {
+            return base.Post(entityToSave);
+        }
+
+        [HttpPatch]
+        [Route("api/v1/professions/{id}", Name = RouteNames.Profession + RouteVerbs.Patch)]
+        public IHttpActionResult Patch(Guid id, JObject entityChanges)
+        {
+            return base.Patch(id, entityChanges);
+        }
+
+        [HttpDelete]
+        [Route("api/v1/professions/{id}", Name = RouteNames.Profession + RouteVerbs.Delete)]
+        public override IHttpActionResult Delete(Guid id)
+        {
+            return base.Delete(id);
         }
     }
 }
