1a248f78c4432be2066410e08cd07a5b9ed63f2b
DDI\mreynolds <mreynolds@ddi.org>
2017-03-06 18:41:09 -0500

    DC-313 added ConstituentPicture model

---
+++ DDI/DDI.Shared/Models/Client/CRM/ConstituentPicture.cs
@@ -0,0 +1,34 @@
+﻿using DDI.Shared.Models.Client.Core;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace DDI.Shared.Models.Client.CRM
+{
+    [Table("ConstituentPicture")]
+    public class ConstituentPicture : EntityBase
+    {
+        #region Public Properties
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public override Guid Id { get; set; }
+
+        public Guid ConstituentId { get; set; }
+
+        public Constituent Constituent { get; set; }
+
+        public Guid FileId { get; set; }
+
+        public FileStorage File { get; set; }
+
+        #endregion Public Properties
+
+        #region Public Methods
+
+        public override string DisplayName => string.Empty;
+
+        #endregion
+    }
+}
