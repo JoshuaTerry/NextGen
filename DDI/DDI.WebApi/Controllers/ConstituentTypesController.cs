c8493223fd6b20dc11a4c5427c0277c5dfaacc08
aeverage <aeverage@ddi.org>
2017-03-15 16:43:47 -0400

    DC-488 relationship category adding fix

--- DDI/DDI.WebApi/Controllers/ConstituentTypesController.cs
+++ DDI/DDI.WebApi/Controllers/ConstituentTypesController.cs
@@ -2,21 +2,50 @@
 using System.Linq.Expressions;
 using System.Web.Http;
 using System.Web.Routing;
+using DDI.Shared.Models.Client.CRM;
 using DDI.Services;
 using DDI.Services.Search;
-using DDI.Shared.Models.Client.CRM;
+using DDI.Services.ServiceInterfaces;
+using DDI.Shared;
+using DDI.Shared.Helpers;
 using DDI.Shared.Statics;
 using Newtonsoft.Json.Linq;
+
 
 namespace DDI.WebApi.Controllers
 {
     public class ConstituentTypesController : ControllerBase<ConstituentType>
     {
+        private string _allFields = null;
+
+        protected override Expression<Func<ConstituentType, object>>[] GetDataIncludesForList()
+        {
+            return new Expression<Func<ConstituentType, object>>[]
+            {
+                a => a.Tags
+            };
+        }
+
+        protected override Expression<Func<ConstituentType, object>>[] GetDataIncludesForSingle()
+        {
+            return new Expression<Func<ConstituentType, object>>[]
+            {
+                c => c.Tags
+            };
+        }
+
         [HttpGet]
         [Route("api/v1/constituenttypes", Name = RouteNames.ConstituentType)]
         public IHttpActionResult GetAll(int? limit = SearchParameters.LimitMax, int? offset = SearchParameters.OffsetDefault, string orderBy = OrderByProperties.DisplayName, string fields = null)
         {
             return base.GetAll(RouteNames.ConstituentType, limit, offset, orderBy, fields);
+        }
+
+        [HttpGet]
+        [Route("api/v1/constituenttypes/{id}", Name = RouteNames.ConstituentType + RouteVerbs.Get)]
+        public IHttpActionResult GetById(Guid id, string fields = null)
+        {
+            return base.GetById(id, fields);
         }
 
         [HttpPost]
@@ -40,36 +69,40 @@ namespace DDI.WebApi.Controllers
             return base.Delete(id);
         }
 
-        protected override Expression<Func<ConstituentType, object>>[] GetDataIncludesForList()
-        {
-            return new Expression<Func<ConstituentType, object>>[]
-            {
-                a => a.Tags
-            };
-        }
+        //protected override string FieldsForAll
+        //{
+        //    get
+        //    {
+        //        if (_allFields == null)
+        //        {
+        //            //// For entity types with recursive properties or large collections, we need to exclude these.
+        //            //// This is an example of using the FieldListBuilder to create a list of fields.
+        //            _allFields = new PathHelper.FieldListBuilder<ConstituentType>()
+        //                .Include(p => p.Code)
+        //                .Include(p => p.Category)
+        //                .Include(p => p.DisplayName)
+        //                .Include(p => p.Id)
+        //                .Include(p => p.IsActive)
+        //                .Include(p => p.IsRequired)
+        //                .Include(p => p.Name)
+        //                .Include(p => p.NameFormat)
+        //                .Include(p => p.SalutationFormal)
+        //                .Include(p => p.SalutationInformal)
+        //                .Include(p => p.Code)
+        //                .Include(p => p.Tags.Count);
 
-        protected override string FieldsForAll
-        {
-            get
-            {
-                if (_allFields == null)
-                {
-                    //// For entity types with recursive properties or large collections, we need to exclude these.
-                    //// This is an example of using the FieldListBuilder to create a list of fields.
-                    _allFields = new PathHelper.FieldListBuilder<ConstituentType>()
-                        .Include(p => p.Tags.ToString());
-                    //    .Exclude(p => p.ReciprocalTypeMale)
-                    //    .Exclude(p => p.FemaleTypes)
-                    //    .Exclude(p => p.MaleTypes)
-                    //    .Exclude(p => p.Relationships)
-                    //    .Exclude(p => p.RelationshipCategory.RelationshipTypes);
-                }
-                return _allFields;
-            }
-        }
+        //            //    .Exclude(p => p.ReciprocalTypeMale)
+        //            //    .Exclude(p => p.FemaleTypes)
+        //            //    .Exclude(p => p.MaleTypes)
+        //            //    .Exclude(p => p.Relationships)
+        //            //    .Exclude(p => p.RelationshipCategory.RelationshipTypes);
+        //        }
+        //        return _allFields;
+        //    }
+        //}
 
         [HttpPost]
-        [Route("api/v1/constituentstypes/{id}/constituenttypetags")]
+        [Route("api/v1/constituenttypes/{id}/constituenttypetags")]
         public IHttpActionResult AddTagsToConstituentType(Guid id, [FromBody] JObject tags)
         {
             try
@@ -82,7 +115,7 @@ namespace DDI.WebApi.Controllers
                 }
 
                 var response = Service.AddTagsToConstituentType(constituentType, tags);
-                
+
                 return Ok(response);
             }
             catch (Exception ex)
