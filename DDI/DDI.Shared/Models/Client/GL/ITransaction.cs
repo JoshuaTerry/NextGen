4d4d29e622f71b6f29d438fd94dcc8ec70b37107
DDI\jterry <jterry@ddi.org>
2017-03-17 15:37:11 -0400

    DC-457

---
+++ DDI/DDI.Shared/Models/Client/GL/ITransaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDI.Shared.Models.Client.GL
+{
+    public interface ITransaction
+    {
+        Guid Id { get; set; }
+        int TransactionId { get; set; }
+        string Description { get; set; }
+        DateTime TransactionDate { get; set; }
+        decimal Amount { get; set; }
+
+        Guid CreditLedgerAccountId { get; set; }
+        [ForeignKey(nameof(CreditLedgerAccountId))]
+        LedgerAccount CreditLedgerAccount { get; set; }
+
+        Guid DebitLedgerAccountId { get; set; }
+        [ForeignKey(nameof(DebitLedgerAccountId))]
+        LedgerAccount DebitLedgerAccount { get; set; }
+    }
+}
