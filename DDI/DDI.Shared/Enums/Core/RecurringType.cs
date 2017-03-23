c18e3bfafa5afc1d244ad29baaa8c5646abcaecd
DDI\jallen <jallen@ddi.org>
2017-03-23 14:33:38 -0400

    DC-457: Move RecurringType to Core

--- DDI/DDI.Shared/Enums/Core/RecurringType.cs
+++ DDI/DDI.Shared/Enums/Core/RecurringType.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DDI.Shared.Enums.GL
+namespace DDI.Shared.Enums.Core
 {
     public enum RecurringType { Monthly = 0, One_Time = 1, On_Hold = 2, Weekly = 3, BiWeekly = 4, Daily = 5, BiMonthly = 6, Quarterly = 7, SemiAnnually = 8, SemiMonthly = 9, Yearly = 10, Period = 11 }
 }
