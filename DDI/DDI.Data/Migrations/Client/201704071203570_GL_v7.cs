723b56b3de11adb0e019d252d8ddb935aa04a96a
Michael Bilyj <mbilyjwork@att.net>
2017-04-07 12:06:51 -0400

    Added Migration file for new migration for Mike, Mike, and Mack

---
+++ DDI/DDI.Data/Migrations/Client/201704071203570_GL_v7.cs
@@ -0,0 +1,19 @@
+namespace DDI.Data.Migrations.Client
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    using Extensions;
+
+    public partial class GL_v7 : DbMigration
+    {
+        public override void Up()
+        {
+           
+        }
+        
+        public override void Down()
+        {
+           
+        }
+    }
+}
