d8fa64467c849b0791796efdbabaf8646128289c
mbower <mbower@ddi.org>
2017-04-12 09:31:01 -0400

    DC-629 added 'skeleton' functions to investmentdetails

--- DDI/DDI.UI.Web/Scripts/InvestmentDetails.js
+++ DDI/DDI.UI.Web/Scripts/InvestmentDetails.js
@@ -1,5 +1,13 @@
 ﻿$(document).ready(function () {
 
+    // investment id will probably be coming from constituent (and elsewhere?)
+    // set in sessionstorage and set hid investment id
+
+    // apply system settings?
+
+    // see roughly line 45 in constituents.js - will have to implement as part of investments tab
+
+
     CreateEditControls();
 
     SetupEditControls();
@@ -17,7 +25,48 @@ function RefreshEntity() {
 function DisplayInvestmentData() {
 
 
+
 }
 
 
+function LoadAutomaticTransactionsSection() {
 
+
+
+}
+
+function LoadAttributesSection() {
+
+
+
+}
+
+function LoadInterestSection() {
+
+
+
+}
+
+function LoadIRSInformationSection() {
+
+
+
+}
+
+function LoadLinkedAccountsSection() {
+
+
+
+}
+
+function LoadMaturitySection() {
+
+
+
+}
+
+function LoadPaymentPreferencesSectioon() {
+
+
+
+}
