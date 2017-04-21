47e04eeb82242197979de4099b655ff9dbe8bd9b
DDI\jallen <jallen@ddi.org>
2017-04-21 10:28:38 -0400

    DC-532: Transaction logic

---
+++ DDI/DDI.Data/Statics/Sequences.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDI.Data.Statics
+{
+    public static class Sequences
+    {
+        public static string TransactionNumber => "Sequence_TransactionNumber";
+    }
+}
