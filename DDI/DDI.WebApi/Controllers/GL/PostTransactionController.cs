d6914fca2354eb603f45f9c1416e348751f144c9
Michael Bilyj <mbilyjwork@att.net>
2017-04-25 11:15:02 -0400

    DC-602 added WebApi Get call for Post Transactions for an account.

---
+++ DDI/DDI.WebApi/Controllers/GL/PostTransactionController.cs
@@ -0,0 +1,45 @@
+﻿using DDI.Services.GL;
+using DDI.Services.Search;
+using DDI.Shared.Models.Client.GL;
+using DDI.Shared.Statics;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using DDI.Services;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Routing;
+
+namespace DDI.WebApi.Controllers.GL
+{
+    public class PostTransactionController : GenericController<PostedTransaction>
+    {
+        private ServiceBase<LedgerAccountYear> _ledgerAccountYear = new ServiceBase<LedgerAccountYear>();
+
+
+        [HttpGet]
+        [Route("api/v1/posttransactions/accountId/{Id}")]
+        public IHttpActionResult GetAllByAccountId(Guid Id)
+        {
+            try
+            {
+                var ledgerAccountYear = _ledgerAccountYear.GetAllWhereExpression(ly=> ly.AccountId == Id).Data.FirstOrDefault();
+                var result = _service.GetAllWhereExpression(pt => pt.LedgerAccountYearId == ledgerAccountYear.Id);
+
+                if (result == null)
+                {
+                    return NotFound();
+                }
+
+                return Ok(result);
+            }
+            catch (Exception ex)
+            {
+                base.Logger.LogError(ex);
+                return InternalServerError(new Exception(ex.Message));
+            }
+        }
+    }
+}
