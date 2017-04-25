2f80892ca400e2c2e1dcb4862d489fc2c1afba9b
Michael Bilyj <mbilyjwork@att.net>
2017-04-25 14:58:22 -0400

    DC-602 added accounttransaction.js and grid js to the project and started on the transaction page.

--- DDI/DDI.UI.Web/Pages/GL/AccountTransactions.aspx
+++ DDI/DDI.UI.Web/Pages/GL/AccountTransactions.aspx
@@ -1,11 +1,16 @@
 ﻿<%@ Page Title="" Language="C#" MasterPageFile="~/TabbedContent.Master" AutoEventWireup="true" CodeBehind="AccountTransactions.aspx.cs" Inherits="DDI.UI.Web.Pages.GL.AccountTransactions" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
-
+    <script type="text/javascript" src ="~/Scripts/accounttransaction.js" ></script>
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
     <h1>Transactions</h1>
+    <br />
+    <div class="gridcontainer">
+
+
+    </div>
 
 </asp:Content>
---
+++ DDI/DDI.UI.Web/Scripts/accounttransaction.js
@@ -0,0 +1,33 @@
+﻿$(document).ready(function () {
+
+
+});
+
+function LoadTransactionGrid(accountId) {
+    var columns = {
+
+    };
+
+    var gridSource = DevExpress.data.AspNet.createStore({
+        key: "",
+        loadUrl: "api/Orders",
+    });
+
+    $('.gridContainer').dxDataGrid({
+        columns:columns,
+        remoteOperations: { paging: true, filtering: true, sorting: true, grouping: true, summary: true, groupPaging: true },
+        dataSource: gridSource ,
+        groupPanel: {
+            allowColumnDragging: true,
+            visible: true
+        },
+        grouping: {
+            autoExpandAll: false,
+        },
+        filterRow: { visible: true },
+        scrolling: {
+            mode: "virtual"
+        },
+        
+    });
+}
