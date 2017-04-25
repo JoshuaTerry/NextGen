dd823d58e27c632fcde4c446ab2051fdd4b0c046
Michael Bilyj <mbilyjwork@att.net>
2017-04-25 11:34:14 -0400

    DC-466 took out comment code

--- DDI/DDI.UI.Web/Scripts/glaccountselection.js
+++ DDI/DDI.UI.Web/Scripts/glaccountselection.js
@@ -1,38 +1,5 @@
 ﻿
 
-//$(document).ready(function () {
-
-//    LoadFiscalYearSelect();
-
-//});
-
-//function LoadFiscalYearSelect() {
-
-//    PopulateDropDown('.as-fiscalyear', 'fiscalyears', '', '');
-
-//}
-
-//function LoadAccountSelectorGrid() {
-
-//    // LedgerId 7BAFBB1E-A2DC-4D85-9542-229378F8DBC7
-//    // FY ID	1A67ED6F-0FD8-47CD-9476-DC09D94E5F28
-
-//    //GLAccountSelector(container, ledgerId, fiscalYearId)
-//    // FiscalYear.Id
-//    // FiscalYear.Ledger.Id
-
-//    // GLAccountSelector('.as-accounts', '7BAFBB1E-A2DC-4D85-9542-229378F8DBC7', '1A67ED6F-0FD8-47CD-9476-DC09D94E5F28');
-//    //MakeServiceCall(method, route, item, successCallback, errorCallback)
-//    MakeServiceCall('GET', 'fiscalyears/' + $('.as-fiscalyear').val(), null, function () {
-
-//        LoadGLAccounts('.as-accounts', data.Data.Id, data.Data.Ledger.Id);
-
-//    }, null);
-
-    
-
-//}
-
 function GLAccountSelector(container, ledgerId, fiscalYearId) {
 
     CreateGLAccountSelector(container)
