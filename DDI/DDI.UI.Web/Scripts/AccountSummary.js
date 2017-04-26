3b0836608a26b3a2f683cdab9ccecf1b61930e87
mbower <mbower@ddi.org>
2017-04-26 08:26:23 -0400

    Dc-606 started loadgrid for account summary

---
+++ DDI/DDI.UI.Web/Scripts/AccountSummary.js
@@ -0,0 +1,19 @@
+﻿$(document).ready(function () {
+
+
+});
+
+
+function LoadAccountSummaryTab() {
+
+    var columns = [
+    { dataField: 'Id', width: '0px', },
+    { dataField: 'StartDate', caption: 'From', dataType: 'date' },
+    { dataField: 'EndDate', caption: 'To', dataType: 'date' },
+    { dataField: 'Name', caption: 'Name' }
+    ];
+
+
+    LoadGrid(); // May not be able to use LoadGrid function
+
+}
