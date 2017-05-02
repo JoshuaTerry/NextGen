6ecf51573e246decf4cd2a878b90e4025f674390
DDI\jallen <jallen@ddi.org>
2017-05-02 12:37:14 -0400

    DC-616: Ensure datepickers are displayed correctly. Fix bug in HttpCacheProvider

--- DDI/DDI.UI.Web/Scripts/constituents.js
+++ DDI/DDI.UI.Web/Scripts/constituents.js
@@ -223,7 +223,7 @@ function DisplayConstituentData() {
 
                     var date = FormatJSONDate(value);
 
-                    $(classname).text(date);
+                    $(classname).val(date);
                 }
 
             }
