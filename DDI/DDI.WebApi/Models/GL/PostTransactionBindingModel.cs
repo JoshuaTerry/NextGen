3f7a5fe49f7fc8b5556748442add69a5e415b676
Michael Bilyj <mbilyjwork@att.net>
2017-05-12 13:01:29 -0400

    DC-602 - added binding model for the Post Transaction grid for account details.

---
+++ DDI/DDI.WebApi/Models/GL/PostTransactionBindingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DDI.Shared.Models.Client.GL;
+
+namespace DDI.WebApi.Models.GL
+{
+    public class PostTransactionBindingModel
+    {
+         public int TotalCount { get; set; }
+         public IEnumerable<PostedTransaction> PostedTransactions { get; set; } 
+         public int GroupCount { get; set; }
+    }
+}
