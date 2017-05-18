39cc385ba73175c104ce96d8b04e4ffbe15d0c8c
mreynolds <mreynolds@ddi.org>
2017-05-18 16:02:52 -0400

    DC-668 started import wizard UI

---
+++ DDI/DDI.UI.Web/Scripts/importwizard.js
@@ -0,0 +1,49 @@
+﻿
+var currentwizpos = 0;
+
+$(document).ready(function () {
+
+    $('.import').click(function (e) {
+        e.preventDefault();
+
+        DisplayImportWizardModal();
+    });
+
+    $('.cancelmodal').click(function (e) {
+
+        e.preventDefault();
+
+        CloseModal(modal);
+
+    });
+
+    $('.nextstep').click(function (e) {
+
+        $(modal).find('.stepsinner').animate({
+            left: '-=618'
+        }, 500);
+
+    });
+
+    $('.prevstep').click(function (e) {
+
+        $(modal).find('.stepsinner').animate({
+            left: '+=618'
+        }, 500);
+
+    });
+
+});
+
+function DisplayImportWizardModal() {
+
+    modal = $('.importwizardmodal').dialog({
+        closeOnEscape: false,
+        modal: true,
+        width: 640,
+        height: 480,
+        resizable: false
+    });
+
+}
+
