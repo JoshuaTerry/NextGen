a5d1defd4c2a2d3c43866a38b08fbfa854724cc2
mbower <mbower@ddi.org>
2017-05-30 13:16:25 -0400

    DC-684 adding unit tests for new routes

---
+++ DDI/DDI.WebApi.Tests/Controllers/GroupControllerTests.cs
@@ -0,0 +1,59 @@
+﻿using DDI.Shared;
+using DDI.Shared.Helpers;
+using DDI.Shared.Models.Client.Security;
+using DDI.WebApi.Controllers.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDI.WebApi.Tests.Controllers
+{
+    [TestClass]
+    public class GroupControllerTests : TestBase
+    {
+        private const string TESTDESCR = "WebApi | Controllers";
+
+        [TestInitialize]
+        public override void Initialize() => base.Initialize();
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void GroupController_GetAll()
+        {
+            var uow = Factory.CreateUnitOfWork();
+            uow.CreateRepositoryForDataSource(SetupRepo());
+
+            GroupController controller = CreateController<GroupController>(uow);
+
+            var result = controller.GetAll();
+
+            var content = GetResponse(result);
+            Assert.AreEqual(3, content.TotalResults, "Correct # of results returned.");
+
+        }
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void GroupController_AddRolesToGroup()
+        {
+
+            // test cases:
+            // add a new role to a group
+            // add an existing role to a group
+            // add an empty collection to a group (?)
+
+        }
+
+
+        private IQueryable<Group> SetupRepo()
+        {
+            var list = new List<Group>();
+            list.Add(new Group { Name = "Group 1", Id = GuidHelper.NewSequentialGuid() });
+            list.Add(new Group { Name = "Group 2", Id = GuidHelper.NewSequentialGuid() });
+            list.Add(new Group { Name = "Group 3", Id = GuidHelper.NewSequentialGuid() });
+
+            return list.AsQueryable();
+        }
+    }
+}
