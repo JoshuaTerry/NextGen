69ab0fd246b884ab200c3b87ab732fa6e704ee6a
DDI\gupadhayay <gupadhayay@ddi.org>
2017-06-08 16:08:28 -0400

    DC-474 Development changes.

--- DDI/DDI.UI.Web/Pages/GL/Journals.aspx
+++ DDI/DDI.UI.Web/Pages/GL/Journals.aspx
@@ -2,10 +2,124 @@
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
 
+    <script type="text/javascript" src="../../Scripts/gljournals.js"></script>
+      <link rel="stylesheet" href="../../CSS/Journals.css" />
+    
+    
+
+   
 </asp:Content>
 
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
 
+     <div class="searchcriteria">
+        
+        <div class="scrollable">
+            <fieldset>
+                <legend>Quick Search</legend>
+
+                <div class="fieldblock">
+                    <input type="text" class="searchquicksearch" />
+                </div>
+
+                                       
+
+               <%-- <div class="accordions nocontrols">--%>
+
+                      <div>
+                        <div class="fieldblock">
+                            <label>Type</label>
+                             <select class="searchtype">
+                                    <option value="0">One-time</option>
+                                    <option value="1">Recurring</option>
+                                    <option value="2">Template</option>
+                                   
+                             </select>
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Journal#</label>
+                            <input type="text" class="searchname" />
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Transaction Date</label>
+                            <input type="text" class="TransactionDate datepicker" />
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>through</label>
+                             <input type="text" class="through datepicker" />
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Created By</label>
+                            <select class="searchcreated"></select>
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Creation Date</label>
+                              <input type="text" class="creationdate datepicker" />
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Status</label>
+                          <select class="searchstatus">
+                                   <option value="1">Active</option>
+                                    <option value="2">Expired</option>
+                                    <option value="4">Approved</option>
+                                    <option value="5">Unapproved</option>
+                                    <option value="6">Posted</option>
+                                    <option value="7">Unposted</option>
+                                     <option value="8">Deleted</option>
+                          </select>
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Journal Memo</label>
+                              <input type="text" class="searchjournalmemo" />
+                        </div>
+
+                        <div class="fieldblock">
+                            <label>Line Item Memo</label>
+                            <input type="text" class="searchLineItemMemo" />
+                        </div>
+
+
+                             
+                  
+
+                </div>
+                  <%--  </div>--%>
+                
+
+            </fieldset>
+        </div>
+
+        <div class="buttons">
+            <input type="button" class="clearsearch" value="Clear" />
+            <input type="button" class="dosearch" value="Search" />
+        </div>
+
+    
+
+<%--     <script type="text/javascript">
+
+        $(document).ready(function () {
+
+            LoadjournalsaccountSettings();
+
+        });
+
+    </script>--%>
+         </div>
+
+    
+    <div class="searchresults">
+        <div class="gridcontainer scrollable"></div>
+    </div>
+      
+  
 
 
 </asp:Content>
---
+++ DDI/DDI.UI.Web/Scripts/gljournals.js
@@ -0,0 +1,201 @@
+﻿/* JOURNALS INQUIRY */
+
+
+$(document).ready(function () {
+
+    Resize();
+
+    
+
+    $('.clearsearch').click(function () {
+        $('.searchcriteria div.fieldblock input').each(function () {
+            $(this).val('');
+        });
+
+        $('.searchcriteria select').each(function () {
+            $(this).val(0);
+        });
+    });
+
+    $(document).keypress(function (e) {
+        if (e.which === 13) {
+            DoSearch();
+        }
+    });
+
+    $('.dosearch').click(function () {
+        DoSearch();
+    });
+
+    $(window).resize(function () {
+        Resize();
+    });
+
+});
+
+function Resize() {
+
+    var windowHeight = $(window).height();
+    var header = $('header').height();
+    var adjustedHeight = (windowHeight - header) - 90;
+
+    $('.searchcriteria div.scrollable').height(adjustedHeight);
+
+    $('.searchresults div.scrollable').height(adjustedHeight + 30);
+}
+
+function AddColumnHeaders() {
+
+    var header = $('.searchresultstable thead');
+    var columns = ['ID', 'Journal#', 'Type', 'Tran Dt', 'Memo', 'Amount', 'Created By', 'Year', 'Status'];
+    var tr = $('<tr>');
+
+    $(columns).each(function () {
+        $('<th>').text(this).appendTo($(tr));
+    });
+
+    $(tr).appendTo($(header));
+
+}
+
+
+function DoSearch() {
+
+    var parameters = GetSearchParameters();
+
+    $.ajax({
+        url: WEB_API_ADDRESS + 'journals/?' + parameters,
+        method: 'GET',
+        contentType: 'application/json; charset-utf-8',
+        dataType: 'json',
+        headers: GetApiHeaders(),
+        crossDomain: true,
+        success: function (data) {
+
+            if (data.Data.length === 1) {
+                DisplayJournals(data.Data[0].ID);
+            }
+            else {
+
+                $('.gridcontainer').dxDataGrid({
+                    dataSource: data.Data,
+                    columns: [
+                        { dataField: 'ID', caption: 'ID', alignment: 'right', width: '100px' },
+                        { dataField: 'JournalNumber', caption: 'Journal#' },
+                        { dataField: 'JournalType', caption: 'Type' },
+                        { dataField: 'TransactionDate', caption: 'Tran Dt' },
+                        { dataField: 'Comment', caption: 'Memo' },
+                        { dataField: 'Amount', caption: 'Amount' },
+                        { dataField: 'CreatedBy', caption: 'Created By' },
+                        { dataField: 'CreatedOn', caption: 'Year' },
+                       
+                        
+                    ],
+                    paging: {
+                        pageSize: 15
+                    },
+                    pager: {
+                        showNavigationButtons: true,
+                        showPageSizeSelector: true,
+                        showInfo: true,
+                        allowedPageSizes: [15, 25, 50, 100]
+                    },
+                    groupPanel: {
+                        visible: true,
+                        allowColumnDragging: true
+                    },
+                    filterRow: {
+                        visible: true,
+                        showOperationChooser: false
+                    },
+                    onRowClick: function (info) {
+                        DisplayConstituent(info.values[0]);
+                    }
+                });
+
+            }
+
+        },
+        error: function (xhr, status, err) {
+            DisplayErrorMessage('Error', xhr.responseJSON.ExceptionMessage);
+        }
+    });
+
+}
+
+function GetSearchParameters() {
+
+    var p = '';
+
+    $('.searchcriteria div.fieldblock input').each(function () {
+        var property = $(this).attr('class').split(' ')[0].replace('search', '');
+        var value = $(this).val();
+
+        if (value) {
+            p += property + '=' + encodeURIComponent(value) + '&';
+        }
+    });
+
+    $('.searchcriteria div.fieldblock select').each(function () {
+        var property = $(this).attr('class').split(' ')[0].replace('search', '');
+        var value = $(this).val();
+
+        if (value && value !== 'null') {
+            p += property + '=' + encodeURIComponent(value) + '&';
+        }
+    });
+
+    p += 'limit=100&';
+    p += 'fields=JournalNumber,JournalType,TransactionDate,Comment,Amount,CreatedBy,CreatedOn,&';
+
+    p = p.substring(0, p.length - 1);
+
+    return p;
+
+}
+
+function DisplayJournals(id) {
+
+    sessionStorage.setItem("ID", id);
+    location.href = "Journals.aspx";
+
+}
+
+
+
+
+function LoadjournalsaccountSettings() {
+
+    var accordion = $('<div>').addClass('accordions');
+    var journals = $('<div>').addClass('journalsinquirycontainer');
+    
+
+    var header = $('<h1>').text('').appendTo($(accordion));
+    $(journals).appendTo($(accordion));
+    PopulatejournalsGrid();
+
+    $(accordion).appendTo($('.gridcontainer'));
+
+    LoadAccordions();
+
+    function PopulatejournalsGrid() {
+        
+        var journalsinquirycolumns = [
+         //   { dataField: 'Appr', width: '0px' },
+            { dataField: 'Id', width: '0px' },
+            { dataField: 'JournalNumber', caption: 'Journal#' },
+            { dataField: 'JournalType', caption: 'Type' },
+            { dataField: 'TransactionDate', caption: 'Tran Dt' },
+            { dataField: 'Comment', caption: 'Memo' },
+            { dataField: 'Amount', caption: 'Amount' },
+            { dataField: 'CreatedBy', caption: 'Created By' },
+            { dataField: 'CreatedOn', caption: 'Year' },
+            { dataField: 'ToLedgerAccount.Name', caption: 'Status' },
+         
+        ];
+
+        LoadGrid('.journalsinquirycontainer', 'journalsinquiry', journalsinquirycolumns, 'journals?fields=all', '', null, '',
+        '.journalsinquirymodal', '', 250, false, false, false, null);
+    }
+
+}
