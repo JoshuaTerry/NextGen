cb3e3ea757cdc0d9966c03d4df8912c92ee1eae2
DDI\jallen <jallen@ddi.org>
2017-06-14 14:51:24 -0400

    DC-468: Fix namespace issue

--- DDI/DDI.Shared/Helpers/EntityFrameworkHelper.cs
+++ DDI/DDI.Shared/Helpers/EntityFrameworkHelper.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Reflection;
 using DDI.Shared.Models;
 
-namespace DDI.Data.Helpers
+namespace DDI.Shared.Helpers
 {
     public static class EntityFrameworkHelper
     {
