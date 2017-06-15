91137fa5a1e5d61f8b7e6c93a5f67372dc4ed4b3
mbower <mbower@ddi.org>
2017-06-15 15:58:24 -0400

    precision 2 fixed in activity + budget tab

--- DDI/DDI.UI.Web/Scripts/accounts.js
+++ DDI/DDI.UI.Web/Scripts/accounts.js
@@ -45,25 +45,25 @@ function LoadAccountActivityAndBudgetTab(id) {
             { dataField: 'PeriodNumber', caption: 'Period Number', visible: false },
             { dataField: 'PeriodName', caption: 'Period Name' },
 
-            { dataField: 'BeginningBalance', caption: 'Beginning Balance', format: 'currency', visible: false},
-            { dataField: 'Debits', caption: 'Debits', format: 'currency' },
-            { dataField: 'Credits', caption: 'Credits', format: 'currency' },
-            { dataField: 'Activity', caption: 'Activity', format: 'currency' },
-            { dataField: 'EndingBalance', caption: 'Ending Balance', format: 'currency' },
+            { dataField: 'BeginningBalance', caption: 'Beginning Balance', format: 'currency', precision: 2, visible: false},
+            { dataField: 'Debits', caption: 'Debits', format: 'currency', precision: 2 },
+            { dataField: 'Credits', caption: 'Credits', format: 'currency', precision: 2 },
+            { dataField: 'Activity', caption: 'Activity', format: 'currency', precision: 2 },
+            { dataField: 'EndingBalance', caption: 'Ending Balance', format: 'currency', precision: 2 },
 
-            { dataField: 'PriorBeginningBalance', caption: 'Prior Beginning Balance', format: 'currency', visible: false },
-            { dataField: 'PriorDebits', caption: 'Prior Debits',  format: 'currency', visible: false },
-            { dataField: 'PriorCredits', caption: 'Prior Credits',  format: 'currency', visible: false },
-            { dataField: 'PriorActivity', caption: 'Prior Activity',  format: 'currency', visible: false },
-            { dataField: 'PriorEndingBalance', caption: 'Prior Ending Balance',  format: 'currency', visible: false },
+            { dataField: 'PriorBeginningBalance', caption: 'Prior Beginning Balance', format: 'currency', precision: 2, visible: false },
+            { dataField: 'PriorDebits', caption: 'Prior Debits', format: 'currency', precision: 2, visible: false },
+            { dataField: 'PriorCredits', caption: 'Prior Credits', format: 'currency', precision: 2, visible: false },
+            { dataField: 'PriorActivity', caption: 'Prior Activity',  format: 'currency', precision: 2, visible: false },
+            { dataField: 'PriorEndingBalance', caption: 'Prior Ending Balance', format: 'currency', precision: 2, visible: false },
 
-            { dataField: 'WorkingBudget', caption: dataActivity.Data.WorkingBudgetName, allowEditing: true, format: 'currency' },
-            { dataField: 'FixedBudget', caption: dataActivity.Data.FixedBudgetName, allowEditing: true, format: 'currency'},
-            { dataField: 'WhatIfBudget', caption: dataActivity.Data.WhatIfBudgetName, allowEditing: true, format: 'currency', visible: false },
+            { dataField: 'WorkingBudget', caption: dataActivity.Data.WorkingBudgetName, allowEditing: true, format: 'currency', precision: 2 },
+            { dataField: 'FixedBudget', caption: dataActivity.Data.FixedBudgetName, allowEditing: true, format: 'currency', precision: 2 },
+            { dataField: 'WhatIfBudget', caption: dataActivity.Data.WhatIfBudgetName, allowEditing: true, format: 'currency', precision: 2, visible: false },
 
-            { dataField: 'WorkingBudgetVariance', caption: 'Working Budget Variance', allowEditing: true, format: 'currency' },
-            { dataField: 'FixedBudgetVariance', caption: 'Fixed Budget Variance', allowEditing: true, format: 'currency', visible: false},
-            { dataField: 'WhatIfBudgetVariance', caption: 'What-If Budget Variance', allowEditing: true, format: 'currency', visible: false}
+            { dataField: 'WorkingBudgetVariance', caption: 'Working Budget Variance', allowEditing: true, format: 'currency', precision: 2 },
+            { dataField: 'FixedBudgetVariance', caption: 'Fixed Budget Variance', allowEditing: true, format: 'currency', precision: 2, visible: false},
+            { dataField: 'WhatIfBudgetVariance', caption: 'What-If Budget Variance', allowEditing: true, format: 'currency', precision: 2, visible: false}
         ];
 
         $('.activitytitle').empty();
