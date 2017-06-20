756da9644369cc71f04958afea32a89765edc9d3
aeverage <aeverage@ddi.org>
2017-06-20 15:18:47 -0400

    DC-650 interim commit

--- DDI/DDI.WebApi.Tests/Controllers/UserControllerTests.cs
+++ DDI/DDI.WebApi.Tests/Controllers/UserControllerTests.cs
@@ -40,28 +40,28 @@ namespace DDI.WebApi.Tests.Controllers
         public override void Initialize() => base.Initialize();
 
 
-        //[TestMethod, TestCategory(TESTDESCR)]
-        //public void UserController_GetAll() //NEW//
-        //{
-        //    var uow = new Mock<IUnitOfWork>();
-        //    uow.Setup(m => m.GetEntities<User>(null)).Returns(SetupRepo());
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void UserController_GetAll() //NEW//
+        {
+            var uow = new Mock<IUnitOfWork>();
+            uow.Setup(m => m.GetEntities<User>(null)).Returns(SetupRepo());
 
-        //    var service = new UserService<User>(uow.Object);
-        //    var controller = new UsersController(service);
+            var service = Factory.CreateService<IService<UserService>>() as UserService;
+            var controller = new UsersController(service);
 
-        //    controller.Request = new HttpRequestMessage();
-        //    controller.Configuration = new HttpConfiguration();
+            controller.Request = new HttpRequestMessage();
+            controller.Configuration = new HttpConfiguration();
 
-        //    IHttpActionResult result = controller.Get();
-        //    var contentResult = result as OkNegotiatedContentResult<IDataResponse>;
+            IHttpActionResult result = controller.Get();
+            var contentResult = result as OkNegotiatedContentResult<IDataResponse>;
 
-        //    var data = ((contentResult.Content as DataResponse<object>).Data as List<ICanTransmogrify>).Cast<User>().ToList();
-        //    Assert.IsNotNull(contentResult, "IDataResponse is returned");
-        //    Assert.IsNotNull(contentResult.Content, "IDataResponse is returned with content");
-        //    Assert.AreEqual(data.Count, 3, "Content is the correct size");
-        //    Assert.AreEqual(data[0].FullName, "John Doe", "Content is accurate");
+            var data = ((contentResult.Content as DataResponse<object>).Data as List<ICanTransmogrify>).Cast<User>().ToList();
+            Assert.IsNotNull(contentResult, "IDataResponse is returned");
+            Assert.IsNotNull(contentResult.Content, "IDataResponse is returned with content");
+            Assert.AreEqual(data.Count, 3, "Content is the correct size");
+            Assert.AreEqual(data[0].FullName, "John Doe", "Content is accurate");
 
-        //}
+        }
 
 
         [TestMethod, TestCategory(TESTDESCR)]
@@ -69,43 +69,48 @@ namespace DDI.WebApi.Tests.Controllers
         {
             var uow = new Mock<IUnitOfWork>();
             uow.Setup(m => m.GetEntities<User>(null)).Returns(SetupRepo());
-
-            IUserService service = new UserService(uow.Object);
+            
+            UserService service = new UserService(uow.Object);
             var controller = new UsersController(service);
 
             controller.Request = new HttpRequestMessage();
             controller.Configuration = new HttpConfiguration();
-            User user = service.GetAll().Data[2];
-
+            User user = service.GetAll().Data.Cast<User>().ToList()[0];
+            
 
             //change default business unit
-            //user.DefaultBusinessUnit = service.GetAll().Data.Cast<User>().ToList()[].;
-            
+            BusinessUnit businessUnit = service.GetAll().Data.Cast<User>().ToList()[2].DefaultBusinessUnit;
+
             //change list of business units
-            //user.BusinessUnits.Add(businessUnits[1]);
+            List<BusinessUnit> businessUnits = service.GetAll().Data.Cast<User>().ToList()[2].BusinessUnits.ToList();
 
             //change group
-            //user.
+            Group group = service.GetAll().Data.Cast<User>().ToList()[1].Groups.ToList()[0];
 
             //change constituent
+            Constituent constituent = service.GetAll().Data.Cast<User>().ToList()[1].Constituent;
 
+            string item = "{ FullName: '" + user.FullName
+                + "', UserName: '" + user.UserName
+                + "', Email: '" + user.Email
+                + "', DefaultBusinessUnitId: '" + businessUnit.Id
+                + "', ConstituentId: '" + constituent.Id
+                + "', GroupId: '" + group.Id
+                + "', BusinessUnitIds: ['" + businessUnits[0].Id + "','" + businessUnits[1].Id + "']"
+                + ", IsActive: '" + true + "' }";
+            JObject jUser = JObject.Parse(item);
 
-            Role role = service.GetAll().Data.Cast<Group>().ToList()[1].Roles.ToList()[0];
-            string roles = "{ Ids: ['" + role.Id.ToString
-                () + "'] }";
-            JObject JRole = JObject.Parse(roles);
+            uow.Setup(m => m.GetById<User>(user.Id, u => u.Groups, u => u.Roles)).Returns(user as User);
+            uow.Setup(m => m.GetById<Group>(group.Id, u => u.Users, u => u.Roles)).Returns(group as Group);
+            uow.Setup(m => m.GetById<User>(user.Id, r => r.BusinessUnits)).Returns(user as User);
+            uow.Setup(m => m.GetById<BusinessUnit>(businessUnit.Id)).Returns(businessUnit as BusinessUnit);
 
-            uow.Setup(m => m.GetById<Group>(group.Id, r => r.Roles)).Returns(group as Group);
-            uow.Setup(m => m.GetById<Group>(group.Id)).Returns(group as Group);
-            uow.Setup(m => m.GetById<Role>(role.Id)).Returns(role);
-            Assert.AreEqual(0, (group as Group).Roles.Count, "No roles to start with");
-
-            IHttpActionResult result = controller.AddRolesToGroup(group.Id, JRole);
+            IHttpActionResult result = controller.Patch(user.Id, jUser);
             var contentResult = (result as OkNegotiatedContentResult<IDataResponse>).Content as DataResponse<object>;
 
             Assert.AreEqual(contentResult.IsSuccessful, true, "Service call was successful");
-            Assert.AreEqual((contentResult.Data as Group).Roles.Count, 1, "One role was added");
-            Assert.AreEqual((contentResult.Data as Group).Roles.Count, 1, "One role was added");
+            Assert.AreEqual((contentResult.Data as User).Roles.Count, 1, "User now has one role");
+            Assert.AreEqual((contentResult.Data as User).BusinessUnits.Count, 2, "User now has two business units");
         }
 
 
@@ -114,7 +119,8 @@ namespace DDI.WebApi.Tests.Controllers
             List<BusinessUnit> businessUnits = new List<BusinessUnit>()
             {
                 new BusinessUnit() { Name = "ABC Unit", Code = "ABC", Id = GuidHelper.NewSequentialGuid() },
-                new BusinessUnit() { Name = "DEF Unit", Code = "DEF", Id = GuidHelper.NewSequentialGuid() }
+                new BusinessUnit() { Name = "DEF Unit", Code = "DEF", Id = GuidHelper.NewSequentialGuid() },
+                new BusinessUnit() { Name = "GHI Unit", Code = "GHI", Id = GuidHelper.NewSequentialGuid() }
             };
 
             List<Constituent> constituents = new List<Constituent>()
@@ -133,14 +139,15 @@ namespace DDI.WebApi.Tests.Controllers
             List<Group> groups = new List<Group>()
             {
                 new Group() { Name = "Test Group", Id = GuidHelper.NewSequentialGuid(), Roles = new List<Role> { roleList[0], roleList[1] } },
-                new Group() { Name = "Second Group", Id = GuidHelper.NewSequentialGuid(), Roles = new List<Role> { roleList[0] } }
+                new Group() { Name = "Second Group", Id = GuidHelper.NewSequentialGuid(), Roles = new List<Role> { roleList[0] } },
+                new Group() { Name = "3rd Group", Id = GuidHelper.NewSequentialGuid(), Roles = new List<Role> { roleList[1] } }
             };
 
             List<User> users = new List<User>()
             {
-                new User { Id = GuidHelper.NewSequentialGuid(), FullName = "John Doe", Email = "JDoe@DDI.org", DefaultBusinessUnit = businessUnits[0], BusinessUnits = new List<BusinessUnit> { businessUnits[0], businessUnits[1] }, Constituent = constituents[0], Groups = new List<Group> { groups[0] } },
-                new User { Id = GuidHelper.NewSequentialGuid(), FullName = "Jack Sprat", Email = "jsprat@DDI.org", DefaultBusinessUnit = businessUnits[1], BusinessUnits = new List<BusinessUnit> { businessUnits[0] }, Constituent = constituents[1] },
-                new User { Id = GuidHelper.NewSequentialGuid(), FullName = "Vanilla Ice", Email = "vice@DDI.org", DefaultBusinessUnit = businessUnits[0], BusinessUnits = new List<BusinessUnit> { businessUnits[0] }, Constituent = constituents[2] }
+                new User { Id = GuidHelper.NewSequentialGuid(), FullName = "John Doe", Email = "JDoe@DDI.org", DefaultBusinessUnit = businessUnits[0], BusinessUnits = new List<BusinessUnit> { businessUnits[0], businessUnits[1], businessUnits[2] }, Constituent = constituents[0], Groups = new List<Group> { groups[0] } },
+                new User { Id = GuidHelper.NewSequentialGuid(), FullName = "Jack Sprat", Email = "jsprat@DDI.org", DefaultBusinessUnit = businessUnits[1], BusinessUnits = new List<BusinessUnit> { businessUnits[1] }, Constituent = constituents[1], Groups = new List<Group> { groups[1] } },
+                new User { Id = GuidHelper.NewSequentialGuid(), FullName = "Vanilla Ice", Email = "vice@DDI.org", DefaultBusinessUnit = businessUnits[2], BusinessUnits = new List<BusinessUnit> { businessUnits[2], businessUnits[1] }, Constituent = constituents[2], Groups = new List<Group> { groups[2] } }
             };
 
             users[0].Roles.Add(new UserRole() { UserId = users[0].Id, RoleId = roleList[0].Id, Id = GuidHelper.NewSequentialGuid()});
@@ -149,7 +156,13 @@ namespace DDI.WebApi.Tests.Controllers
             users[0].Roles.Add(new UserRole() { UserId = users[0].Id, RoleId = roleList[1].Id });
             roleList[1].Users.Add(new UserRole { RoleId = roleList[1].Id, UserId = users[0].Id });
 
-
+            groups[0].Users = new List<User>();
+            groups[0].Users.Add(users[0]);
+            groups[1].Users = new List<User>();
+            groups[1].Users.Add(users[1]);
+            groups[2].Users = new List<User>();
+            groups[2].Users.Add(users[2]);
+            
             return users.AsQueryable<User>();
         }
 
