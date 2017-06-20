373222733bdc2e05271520d830e9895b99bf1df8
DDI\jterry <jterry@ddi.org>
2017-06-20 12:37:28 -0400

    dc-782 test update

--- DDI/DDI.Business.Tests/GL/AccountLogicTest.cs
+++ DDI/DDI.Business.Tests/GL/AccountLogicTest.cs
@@ -466,41 +466,6 @@ namespace DDI.Business.Tests.GL
             pamounts.Amount14 = value;
 
             return pamounts;
-        }
-        //[TestClass]
-        //public class CountriesControllerTests
-        //{
-        //    private const string TESTDESCR = "WebApi | Controllers";
-
-        //    [TestMethod, TestCategory(TESTDESCR)]
-        //    public void GetAllCountriesnAll()
-        //    {
-        //        var uow = new Mock<IUnitOfWork>();
-        //        uow.Setup(m => m.GetEntities<Country>(null)).Returns(SetupRepo());
-
-        //        var service = new ServiceBase<Country>(uow.Object);
-        //        var controller = new CountriesController(service);
-        //        controller.Request = new HttpRequestMessage();
-        //        controller.Configuration = new HttpConfiguration();
-
-        //        IHttpActionResult result = controller.GetAll();
-        //        var contentResult = result as OkNegotiatedContentResult<IDataResponse>;
-
-        //        Assert.IsNotNull(contentResult);
-        //        Assert.IsNotNull(contentResult.Content);
-        //        Assert.AreEqual(((contentResult.Content as DataResponse<object>).Data as List<DDI.Shared.Models.ICanTransmogrify>).Count, 3);
-        //    }
-        //    private IQueryable<Country> SetupRepo()
-        //    {
-        //        var list = new List<Country>();
-        //        list.Add(new Country { CountryCode = "US", Description = "Merica" });
-        //        list.Add(new Country { CountryCode = "CA", Description = "Canada" });
-        //        list.Add(new Country { CountryCode = "Mx", Description = "Mexico" });
-
-        //        var response = list.AsQueryable<Country>();
-        //        return response;
-        //    }
-        //}
-
+        } 
     }
 }
