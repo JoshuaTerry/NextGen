2a8842849ca1129c4dfe2ebf8743a3b4868630d6
Michael Bilyj <mbilyjwork@att.net>
2017-06-29 13:29:38 -0400

    DC-751 final change for code review. moved the file type code to the model and moved the controller back to a base getbyid call. now the FileType property has a method to return FileType using the extension of that file.

--- DDI/DDI.WebApi/Controllers/Core/FileStorageController.cs
+++ DDI/DDI.WebApi/Controllers/Core/FileStorageController.cs
@@ -28,18 +28,8 @@ namespace DDI.WebApi.Controllers.General
         public IHttpActionResult GetById(Guid id, string fields = null)
         {
             fields = "Id,Name,Extension,Size,Data,FileType";
-            var response = Service.GetById(id);
-            response.Data.FileType = ReturnExtension(response.Data.Extension);
-            return FinalizeResponse(response, fields, null);
+            return base.GetById(id, fields, null);
         }
-        [HttpGet]
-        [Route("api/v1/filestorage/fileextensions/{extension}")]
-        public IHttpActionResult GetFileTypeByExtension(string extension)
-        {
-            string fileType = ReturnExtension(extension);
-            return Ok(fileType);
-        }
-
         [HttpPost]
         [Route("api/v1/filestorage")]
         public IHttpActionResult Post([FromBody] FileStorage entityToSave)
@@ -109,86 +99,5 @@ namespace DDI.WebApi.Controllers.General
             return base.Delete(id);
         }
 
-        private string ReturnExtension(string fileExtension)
-        {
-            switch (fileExtension.ToLower())
-            {
-                case "html":
-                    return "text/HTML";
-                case "asc":
-                case "csv":
-                case "dat":
-                case "df":
-                case "txt":
-                    return "text/plain";
-                case "doc":
-                case "docm":
-                case "docx":
-                    return "application/ms-word";
-                case "tiff":
-                case "tif":
-                    return "image/tiff";
-                case "avi":
-                    return "video/avi";
-                case "zip":
-                    return "application/zip";
-                case "slk":
-                case "xls":
-                case "xlsb":
-                case "xlsm":
-                case "xlsx":
-                    return "application/x-msexcel";
-                case "gif":
-                    return "image/gif";
-                case "jpe":
-                case "jpg":
-                case "jpeg":
-                    return "image/jpeg";
-                case "bmp":
-                    return "image/bmp";
-                case "mp3":
-                    return "audio/mpeg3";
-                case "htm":
-                    return "application/rtf";
-                case "rtf":
-                    return "text/rtf";
-                case "css":
-                    return "text/css";
-                case "pdf":
-                    return "application/pdf";
-                case "msg":
-                    return "application/vnd.ms-outlook";
-                case "ppam":
-                case "pps":
-                case "ppsm":
-                case "ppsx":
-                case "ppt":
-                case "pptm":
-                case "pptx":
-                case "ppz":
-                    return "application/mspowerpoint";
-                case "dwg":
-                    return "image/vnd.dwg";
-                case "ps":
-                    return "application/postscript";
-                case "wp":
-                case "wp5":
-                case "wp6":
-                case "wpd":
-                    return "application/workdperfect";
-                case "png":
-                    return "image/png";
-                case "mht":
-                    return "message/rfc822";
-                case "rtx":
-                    return "text/richtext";
-                case "xml":
-                case "xsd":
-                case "xsl":
-                    return "text/xml";
-                default:
-                    return null;
-            }
-        }
     }
 }
