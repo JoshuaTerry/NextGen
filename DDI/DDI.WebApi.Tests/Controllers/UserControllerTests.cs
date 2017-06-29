10f8ec536c22229eb25dd0170e8c8c8b9494f384
DDI\jterry <jterry@ddi.org>
2017-06-29 10:31:37 -0400

    dc-450 Merge

--- DDI/DDI.WebApi.Tests/Controllers/UserControllerTests.cs
+++ DDI/DDI.WebApi.Tests/Controllers/UserControllerTests.cs
@@ -1,30 +1,16 @@
-﻿using DDI.Services;
-using DDI.Services.General;
-using DDI.Services.Security;
-using DDI.Services.ServiceInterfaces;
+﻿using DDI.Services.Security;
 using DDI.Shared;
 using DDI.Shared.Helpers;
 using DDI.Shared.Models;
 using DDI.Shared.Models.Client.CRM;
 using DDI.Shared.Models.Client.GL;
 using DDI.Shared.Models.Client.Security;
-using DDI.Shared.Models.Common;
-using DDI.WebApi.Controllers.Core;
-using DDI.WebApi.Controllers.CRM;
 using DDI.WebApi.Controllers.General;
-using DDI.WebApi.Models.BindingModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
-using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
-using System.Threading.Tasks;
 using System.Web.Http;
 using System.Web.Http.Results;
 
@@ -34,91 +20,10 @@ namespace DDI.WebApi.Tests.Controllers
     public class UserControllerTests : TestBase
     {
         private const string TESTDESCR = "WebApi | Controllers";
-        private User user;
 
         [TestInitialize]
         public override void Initialize() => base.Initialize();
-
-
-        [TestMethod, TestCategory(TESTDESCR)]
-        public void UserController_GetAll() //NEW//
-        {
-            var uow = new Mock<IUnitOfWork>();
-            uow.Setup(m => m.GetEntities<User>(null)).Returns(SetupRepo());
-
-            var service = Factory.CreateService<IService<UserService>>() as UserService;
-            var controller = new UsersController(service);
-
-            controller.Request = new HttpRequestMessage();
-            controller.Configuration = new HttpConfiguration();
-
-            IHttpActionResult result = controller.GetAll();
-            var contentResult = result as OkNegotiatedContentResult<IDataResponse>;
-
-            var data = ((contentResult.Content as DataResponse<object>).Data as List<ICanTransmogrify>).Cast<User>().ToList();
-            Assert.IsNotNull(contentResult, "IDataResponse is returned");
-            Assert.IsNotNull(contentResult.Content, "IDataResponse is returned with content");
-            Assert.AreEqual(data.Count, 3, "Content is the correct size");
-            Assert.AreEqual(data[0].FullName, "John Doe", "Content is accurate");
-
-        }
-
-
-        [TestMethod, TestCategory(TESTDESCR)]
-        public void UserController_EditUser()
-        {
-            var uow = new Mock<IUnitOfWork>();
-            uow.Setup(m => m.GetEntities<User>(null)).Returns(SetupRepo());
             
-            UserService service = new UserService(uow.Object);
-            var controller = new UsersController(service);
-
-            controller.Request = new HttpRequestMessage();
-            controller.Configuration = new HttpConfiguration();
-            User user = service.GetAll().Data.Cast<User>().ToList()[0];
-            
-
-            //change default business unit
-            BusinessUnit businessUnit = service.GetAll().Data.Cast<User>().ToList()[2].DefaultBusinessUnit;
-
-            //change list of business units
-            List<BusinessUnit> businessUnits = service.GetAll().Data.Cast<User>().ToList()[2].BusinessUnits.ToList();
-
-            //change group
-            Group group = service.GetAll().Data.Cast<User>().ToList()[1].Groups.ToList()[0];
-
-            //change constituent
-            Constituent constituent = service.GetAll().Data.Cast<User>().ToList()[1].Constituent;
-
-            string item = "{ FullName: '" + user.FullName
-                + "', UserName: '" + user.UserName
-                + "', Email: '" + user.Email
-                + "', DefaultBusinessUnitId: '" + businessUnit.Id
-                + "', ConstituentId: '" + constituent.Id
-                + "', GroupId: '" + group.Id
-                + "', BusinessUnitIds: ['" + businessUnits[0].Id + "','" + businessUnits[1].Id + "']"
-                + ", IsActive: '" + true + "' }";
-            JObject jUser = JObject.Parse(item);
-
-            uow.Setup(m => m.GetById<User>(user.Id, u => u.Groups, u => u.Roles)).Returns(user as User);
-            uow.Setup(m => m.GetById<Group>(group.Id, u => u.Users, u => u.Roles)).Returns(group as Group);
-            uow.Setup(m => m.GetById<User>(user.Id, r => r.BusinessUnits)).Returns(user as User);
-            uow.Setup(m => m.GetById<BusinessUnit>(businessUnit.Id)).Returns(businessUnit as BusinessUnit);
-
-
-            // There is no Patch Method on the controller.  There is an update, but the update you have takes a User and not a jObject 
-            // So I'm not entirely sure if I understand what this should call
-            //IHttpActionResult result = controller.Patch(user.Id, jUser);
-
-
-            //var contentResult = (result as OkNegotiatedContentResult<IDataResponse>).Content as DataResponse<object>;
-
-            //Assert.AreEqual(contentResult.IsSuccessful, true, "Service call was successful");
-            //Assert.AreEqual((contentResult.Data as User).Roles.Count, 1, "User now has one role");
-            //Assert.AreEqual((contentResult.Data as User).BusinessUnits.Count, 2, "User now has two business units");
-        }
-
-
         private IQueryable<User> SetupRepo()
         {
             List<BusinessUnit> businessUnits = new List<BusinessUnit>()
