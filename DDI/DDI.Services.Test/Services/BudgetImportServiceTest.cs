fa29bdd12fe0cb3e2a8d2f058bb7c65fff23687b
DDI\jterry <jterry@ddi.org>
2017-07-18 08:21:07 -0400

    dc-669 Conversion Problem

--- DDI/DDI.Services.Test/Services/BudgetImportServiceTest.cs
+++ DDI/DDI.Services.Test/Services/BudgetImportServiceTest.cs
@@ -1,9 +1,13 @@
 ﻿using DDI.Services.GL;
 using DDI.Shared;
+using DDI.Shared.Enums.GL;
 using DDI.Shared.Models.Client.Core;
+using DDI.Shared.Models.Client.GL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 
@@ -13,35 +17,137 @@ namespace DDI.Services.Test.Services
     public class BudgetImportServiceTest
     {
         private const string TESTDESCR = "Services | BudgetImport";
+        private Guid importFileId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6922");
+        private Guid fiscalYearId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6923");
+        private Guid ledgerAccountId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6924");
+        private Guid ledgerId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6925");
+        private Guid accountId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6926");
 
+        private Guid fixedBudgetId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6927");
+        private Guid workingBudgetId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6928");
+        private Guid whatIfBudgetId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6929");
+        private string accountNumber = "01-305-45-60";
+
+
+        // Right Click in this function and select Debug Test.  Set a break point in BudgetImportService.cs at line 84
         [TestMethod, TestCategory(TESTDESCR)]
         public void ImportBudgets_OneBudgetImported()
         {
-            Guid groupId = Guid.Parse("D7ABB652-B6D7-47AF-A945-B25DA5FD6922");
+            var uow = Setup();
 
-            var uow = new Mock<IUnitOfWork>();
-            uow.Setup(m => m.GetById<ImportFile>(It.IsAny<Guid>(), It.IsAny<Expression<Func<ImportFile, object>>[]>())).Returns(CreateImportFile());
+            var service = new BudgetImportService(uow);
 
+            var accountColumn = new MappableEntityField() { ColumnName = "Account", PropertyName = "AccountNumber" };
+            var budgetColumn = new MappableEntityField() { ColumnName = "Budget", PropertyName = "Budget" };
+            var period1Column = new MappableEntityField() { ColumnName = "Period 1", PropertyName = "Amount01" };
+            var fields = new[] { accountColumn, budgetColumn, period1Column };
 
-            var service = new BudgetImportService(uow.Object);
+            var response = service.ImportBudgets(importFileId, fiscalYearId, fields);
+
+            Assert.AreEqual(true, response.IsSuccessful);
         }
 
-        private ImportFile CreateImportFile()
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void GetBugetNames_Returns3BudgetNames()
+        {
+            var uow = Setup();
+
+            var service = new BudgetImportService(uow);
+            (string fix, string working, string whatif) = service.GetBudgetNames(GetAccount());
+
+            Assert.AreEqual("Eeny", fix);
+            Assert.AreEqual("Meeny", working);
+            Assert.AreEqual("Miny", whatif);
+        }
+
+        [TestMethod, TestCategory(TESTDESCR)]
+        public void GetAccountBudget_ReturnsValidBudgetForAlias()
+        {
+            var uow = Setup();
+
+            var service = new BudgetImportService(uow);
+            var result = service.GetAccountBudget(accountNumber, fiscalYearId, "Miny");
+            Assert.IsNotNull(result);
+        }
+
+        private IUnitOfWork Setup()
+        {
+            var uow = new Mock<IUnitOfWork>();
+            uow.Setup(m => m.GetById<ImportFile>(It.IsAny<Guid>(), It.IsAny<Expression<Func<ImportFile, object>>[]>())).Returns(GetImportFile());
+            uow.Setup(m => m.GetEntities<Account>(It.IsAny<Expression<Func<Account, object>>[]>())).Returns(GetAccountQueryable());
+            uow.Setup(m => m.GetEntities<AccountBudget>()).Returns(GetAccountBudgets().AsQueryable());
+            uow.Setup(m => m.GetEntities<LedgerAccount>()).Returns(new List<LedgerAccount>(new[] { GetLedgerAccount() }).AsQueryable());
+            uow.Setup(m => m.GetEntities<Ledger>()).Returns(new List<Ledger>(new[] { GetLedger() }).AsQueryable());
+
+            return uow.Object;
+        }
+        private ImportFile GetImportFile()
         {
             var importFile = new ImportFile();
+            importFile.Id = importFileId;
             var file = new FileStorage();
 
             var contents = new StringBuilder();
             contents.AppendLine("Business Unit,Fiscal Year,Account,Budget,Period 1,Period 2,Period 3,Period 4,Period 5,Period 6,Period 7,Period 8,Period 9,Period 10,Period 11,Period 12,Period 13");
-            contents.AppendLine("DCEF,2016,01-305-45-60,What If Budget,12.54,45.78,-65.78,0.78,0,0,0,0,0,0,0,0,0");
+            contents.AppendLine("DCEF,2016,01-305-45-60,Miny,12.54,45.78,-65.78,0.78,0,0,0,0,0,0,0,0,0");
             file.Data = Encoding.ASCII.GetBytes(contents.ToString());
             file.Extension = "csv";
             file.FileType = "csv";
             file.Name = "TestFile";
 
+            importFile.File = file;
 
+            return importFile;
+        }
 
-            return null;
+        private IQueryable<Account> GetAccountQueryable()
+        {
+            var account = GetAccount();
+            var list = new List<Account>();
+            list.Add(account);
+            return list.AsQueryable();
+
+        }
+        private Account GetAccount()
+        {
+            var account = new Account();
+            account.Id = accountId;
+            account.AccountNumber = accountNumber;
+            account.FiscalYearId = fiscalYearId;
+            account.Budgets = GetAccountBudgets();
+
+            return account;
+        }
+
+        private LedgerAccount GetLedgerAccount()
+        {
+            var ledgerAccount = new LedgerAccount();
+            ledgerAccount.Id = ledgerAccountId;
+            ledgerAccount.LedgerId = ledgerId;
+            ledgerAccount.AccountNumber = accountNumber;
+
+            return ledgerAccount;
+        }
+
+        private Ledger GetLedger()
+        {
+            var ledger = new Ledger();
+            ledger.Id = ledgerId;
+            ledger.FixedBudgetName = "Eeny";
+            ledger.WorkingBudgetName = "Meeny";
+            ledger.WhatIfBudgetName = "Miny";
+
+            return ledger;
+        }
+
+        private List<AccountBudget> GetAccountBudgets()
+        {
+            var list = new List<AccountBudget>();
+            list.Add(new AccountBudget() { Id = fixedBudgetId, AccountId = accountId, BudgetType = BudgetType.Fixed, Budget = new PeriodAmountList() });
+            list.Add(new AccountBudget() { Id = workingBudgetId, AccountId = accountId, BudgetType = BudgetType.Working, Budget = new PeriodAmountList() });
+            list.Add(new AccountBudget() { Id = whatIfBudgetId, AccountId = accountId, BudgetType = BudgetType.WhatIf, Budget = new PeriodAmountList() });
+
+            return list;
         }
     }
 }
--- DDI/Services/GL/BudgetImportService.cs
+++ DDI/Services/GL/BudgetImportService.cs
@@ -4,12 +4,32 @@ using DDI.Shared.Models.Client.Core;
 using DDI.Shared.Models.Client.GL;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Linq.Expressions;
 
 namespace DDI.Services.GL
 {
+    public static class ExpressionCastingTest
+    {
+        public static object Cast(this Type Type, object data)
+        {
+            var parm = Expression.Parameter(typeof(object), "data");
+            var body = Expression.Block(GetExpression(parm, data, Type));
+
+            var run = Expression.Lambda(body, parm).Compile();
+            var value = run.DynamicInvoke(data);
+
+            return value;
+        }
+        private static UnaryExpression GetExpression(ParameterExpression pe, object data, Type type)
+        {
+            // Throws System.InvalidOperationException: 'No coercion operator is defined between types 'System.String' and 'System.Decimal'.'
+            // Only happens with decimal
+            return Expression.Convert(Expression.Convert(pe, data.GetType()), type);
+        }
+    }
     public class BudgetImportService
     {
         private IUnitOfWork _uow = null;
@@ -27,7 +47,7 @@ namespace DDI.Services.GL
         {
             var response = new DataResponse();
             var importFile = GetImportFile(fileId);
-            if (!fields.Any(f => f.PropertyName == "Account"))
+            if (!fields.Any(f => f.PropertyName == "AccountNumber"))
                 throw new Exception("No Account Mapping Specified.");
 
             char[] delimiters = { ',' };
@@ -49,18 +69,36 @@ namespace DDI.Services.GL
                         string[] values = line.Split(delimiters, StringSplitOptions.None);
 
                         var propertyValues = mappings.Select(m => new { Property = m.PropertyName, Value = values[m.Ordinal] }).ToList();
-                        var accountNumber = propertyValues.FirstOrDefault(p => p.Property == "Account");
+                        var accountNumber = propertyValues.FirstOrDefault(p => p.Property == "AccountNumber");
                         var budgetName = propertyValues.FirstOrDefault(p => p.Property == "Budget");
                         var budget = GetAccountBudget(accountNumber.Value, fiscalYearId, budgetName.Value);
 
                         if (budget != null)
                         {
-                            var remainingValues = propertyValues.Where(pv => pv.Property != "Account" && pv.Property != "Budget").ToList();
+                            var remainingValues = propertyValues.Where(pv => pv.Property != "AccountNumber" && pv.Property != "Budget").ToList();
+
                             foreach (var item in propertyValues)
                             {
-                                var budgetType = typeof(AccountBudget);
+                                var budgetType = typeof(PeriodAmountList);
                                 var property = budgetType.GetProperty(item.Property);
-                                property.SetValue(budget, Convert.ChangeType(item.Value, property.PropertyType));
+                                if (property != null)
+                                {
+                                    // Throws System.Reflection.TargetException: 'Object does not match target type.'
+                                    // This is because ConvertValue has to return as Object in order to be dynamic
+                                    //property.SetValue(budget, ConvertValue(item.Value, property.PropertyType)); 
+
+                                    // Throws System.MissingMethodException: 'Constructor on type 'System.Decimal' not found.'
+                                    // This is because there is no accessibe default contructor for type decimal                                    
+                                    //property.SetValue(budget, Activator.CreateInstance(property.PropertyType, item.Value));  
+
+                                    // Throws System.InvalidOperationException: 'No coercion operator is defined between types 'System.String' and 'System.Decimal'.'
+                                    // This uses the exetension method defined at the top of this class.  I don't understand why it doesn't work.                                    
+                                    //property.SetValue(budget, property.PropertyType.Cast(item.Value));
+
+                                    // Throws System.Reflection.TargetException: 'Object does not match target type
+                                    // This is supposed to work on any type that implements IConvertable which both String and Decimal do.  I don't understand why it doesn't work.                                  
+                                    //property.SetValue(budget, Convert.ChangeType(item.Value, property.PropertyType, CultureInfo.InvariantCulture));
+                                }
                             }
                             _uow.Update(budget);
                             _uow.SaveChanges();
@@ -71,7 +109,22 @@ namespace DDI.Services.GL
             return response;
         }
 
-        private AccountBudget GetAccountBudget(string accountNumber, Guid fiscalYearId, string budgetName)
+        private object ConvertValue(object value, Type type)
+        {
+            object newValue = Convert.ToString(value);
+            if (type == typeof(decimal))
+            {
+                newValue = decimal.Parse(Convert.ToString(value));
+            }
+            else
+            {
+                newValue = Convert.ChangeType(value, type, CultureInfo.InvariantCulture);
+            }
+
+            return newValue;
+        }
+
+        internal AccountBudget GetAccountBudget(string accountNumber, Guid fiscalYearId, string budgetName)
         {
             AccountBudget budget = null;
             var account = GetAccount(accountNumber, fiscalYearId);
@@ -97,7 +150,7 @@ namespace DDI.Services.GL
             }
             return budget;
         }
-        private (string, string, string) GetBudgetNames(Account account)
+        internal (string, string, string) GetBudgetNames(Account account)
         {
             string fixedBudget = string.Empty;
             string workingBudget = string.Empty;
@@ -116,17 +169,18 @@ namespace DDI.Services.GL
             }
             return (fixedBudget, workingBudget, whatIfBudget);
         }
-        private List<AccountBudget> GetAccountBudgets(Account account)
+        internal List<AccountBudget> GetAccountBudgets(Account account)
         {
+            var includes = new Expression<Func<AccountBudget, object>>[] { a => a.Budget };
             return _uow.GetEntities<AccountBudget>().Where(ab => ab.AccountId == account.Id).ToList();
 
         }
-        private Account GetAccount(string accountNumber, Guid fiscalYearId)
+        internal Account GetAccount(string accountNumber, Guid fiscalYearId)
         {
             var includes = new Expression<Func<Account, object>>[] { a => a.Budgets };
             return _uow.GetEntities<Account>(includes).FirstOrDefault(a => a.AccountNumber == accountNumber && a.FiscalYearId == fiscalYearId);
         }
-        private ImportFile GetImportFile(Guid id)
+        internal ImportFile GetImportFile(Guid id)
         {
             var includes = new Expression<Func<ImportFile, object>>[] { f => f.File, f => f.File.Data };
             var importFile = _uow.GetById<ImportFile>(id, includes);
